c5ea1ff72a25ce418b0c0861c6d2628e8614dfab
Amitay Horwitz <amitayh@wix.com>
2020-02-23 12:41:02 +0200

    Copybara (#8844)
    GitOrigin-RevId: 129dfb54a54647af55df8182e1c84f68c42080d7

---
+++ future-interop/src/main/scala/com/wixpress/dst/greyhound/future/GreyhoundProducerConfig.scala
@@ -0,0 +1,3 @@
+package com.wixpress.dst.greyhound.future
+
+case class GreyhoundProducerConfig()
--- java-interop/src/main/java/com/wixpress/dst/greyhound/java/RecordHandler.java
+++ java-interop/src/main/java/com/wixpress/dst/greyhound/java/RecordHandler.java
@@ -3,10 +3,9 @@ package com.wixpress.dst.greyhound.java;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 
 public interface RecordHandler<K, V> {
 
-    CompletableFuture<Void> handle(ConsumerRecord<K, V> record, Executor executor);
+    CompletableFuture<Void> handle(ConsumerRecord<K, V> record);
 
 }
