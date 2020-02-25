ad40395ed5f564b68c47d36a7a5e6ad14eeddb1a
Amitay Horwitz <amitayh@wix.com>
2020-02-25 16:19:03 +0200

    Separate consumers from producers (#8910)
    GitOrigin-RevId: dd62a891afe979f16da5d273e918319a9edee252

--- future-interop/src/main/scala/com/wixpress/dst/greyhound/future/GreyhoundProducer.scala
+++ future-interop/src/main/scala/com/wixpress/dst/greyhound/future/GreyhoundProducer.scala
@@ -1,7 +1,9 @@
 package com.wixpress.dst.greyhound.future
 
 import com.wixpress.dst.greyhound.core.Serializer
-import com.wixpress.dst.greyhound.core.producer.{ProducerRecord, RecordMetadata}
+import com.wixpress.dst.greyhound.core.producer._
+import com.wixpress.dst.greyhound.future.GreyhoundRuntime.Env
+import zio.{Promise, ZIO}
 
 import scala.concurrent.Future
 
@@ -9,4 +11,33 @@ trait GreyhoundProducer {
   def produce[K, V](record: ProducerRecord[K, V],
                     keySerializer: Serializer[K],
                     valueSerializer: Serializer[V]): Future[RecordMetadata]
+
+  def shutdown: Future[Unit]
+}
+
+case class GreyhoundProducerBuilder(config: GreyhoundConfig) {
+  def build: Future[GreyhoundProducer] = config.runtime.unsafeRunToFuture {
+    for {
+      shutdownSignal <- Promise.make[Nothing, Unit]
+      promise <- Promise.make[Nothing, Producer[Env]]
+      fiber <- Producer.make(ProducerConfig(config.bootstrapServers)).use { producer =>
+        promise.succeed(ReportingProducer(producer)) *>
+          shutdownSignal.await
+      }.fork
+      runtime <- ZIO.runtime[Env]
+    } yield new GreyhoundProducer {
+      override def produce[K, V](record: ProducerRecord[K, V],
+                                 keySerializer: Serializer[K],
+                                 valueSerializer: Serializer[V]): Future[RecordMetadata] =
+        runtime.unsafeRunToFuture {
+          promise.await.flatMap { producer =>
+            producer.produce(record, keySerializer, valueSerializer)
+          }
+        }
+
+      override def shutdown: Future[Unit] = runtime.unsafeRunToFuture {
+        shutdownSignal.succeed(()) *> fiber.join
+      }
+    }
+  }
 }
