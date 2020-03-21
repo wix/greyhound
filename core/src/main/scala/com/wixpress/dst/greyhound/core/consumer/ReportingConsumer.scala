cc08b4803ca9f2bed0bdafb3f78dadf293fe86fd
Noam Berman <noambe@wix.com>
2020-03-21 15:11:04 +0200

    Greyhound ng visibility (#9359)
    * greyhound ng: add missing visibility
    
    GitOrigin-RevId: f18ba407c4cc8a7bd7273acdf98bf40d81759597

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/ReportingConsumer.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/ReportingConsumer.scala
@@ -30,15 +30,18 @@ case class ReportingConsumer[R](clientId: ClientId, group: Group, internal: Cons
     } yield ()
 
   override def poll(timeout: Duration): RIO[R with GreyhoundMetrics, Records] =
-    internal.poll(timeout).tapError { error =>
-      Metrics.report(PollingFailed(clientId, group, error))
-    }
+    for {
+      records <- internal.poll(timeout).tapError { error =>
+        Metrics.report(PollingFailed(clientId, group, error))
+      }
+      _ <- Metrics.report(PolledRecords(clientId, group, records)).as(records)
+    } yield records
 
   override def commit(offsets: Map[TopicPartition, Offset], calledOnRebalance: Boolean): RIO[R with GreyhoundMetrics, Unit] =
     ZIO.when(offsets.nonEmpty) {
       Metrics.report(CommittingOffsets(clientId, group, offsets, calledOnRebalance)) *>
         internal.commit(offsets, calledOnRebalance).tapError { error =>
-          Metrics.report(CommitFailed(clientId, group, error))
+          Metrics.report(CommitFailed(clientId, group, error, offsets))
         }
     }
 
@@ -46,7 +49,7 @@ case class ReportingConsumer[R](clientId: ClientId, group: Group, internal: Cons
     ZIO.when(partitions.nonEmpty) {
       Metrics.report(PausingPartitions(clientId, group, partitions)) *>
         internal.pause(partitions).tapError { error =>
-          Metrics.report(PausePartitionsFailed(clientId, group, error))
+          Metrics.report(PausePartitionsFailed(clientId, group, error, partitions))
         }
     }
 
@@ -54,35 +57,52 @@ case class ReportingConsumer[R](clientId: ClientId, group: Group, internal: Cons
     ZIO.when(partitions.nonEmpty) {
       Metrics.report(ResumingPartitions(clientId, group, partitions)) *>
         internal.resume(partitions).tapError { error =>
-          Metrics.report(ResumePartitionsFailed(clientId, group, error))
+          Metrics.report(ResumePartitionsFailed(clientId, group, error, partitions))
         }
     }
 
   override def seek(partition: TopicPartition, offset: Offset): ZIO[R with GreyhoundMetrics, IllegalStateException, Unit] =
     Metrics.report(SeekingToOffset(clientId, group, partition, offset)) *>
       internal.seek(partition, offset).tapError { error =>
-        Metrics.report(SeekToOffsetFailed(clientId, group, error))
+        Metrics.report(SeekToOffsetFailed(clientId, group, error, partition, offset))
       }
 
 }
 
 sealed trait ConsumerMetric extends GreyhoundMetric {
   def clientId: ClientId
+
   def group: Group
 }
 
 object ConsumerMetric {
+
   case class SubscribingToTopics(clientId: ClientId, group: Group, topics: Set[Topic]) extends ConsumerMetric
+
   case class CommittingOffsets(clientId: ClientId, group: Group, offsets: Map[TopicPartition, Offset], calledOnRebalance: Boolean) extends ConsumerMetric
+
   case class PausingPartitions(clientId: ClientId, group: Group, partitions: Set[TopicPartition]) extends ConsumerMetric
+
   case class ResumingPartitions(clientId: ClientId, group: Group, partitions: Set[TopicPartition]) extends ConsumerMetric
+
   case class SeekingToOffset(clientId: ClientId, group: Group, partition: TopicPartition, offset: Offset) extends ConsumerMetric
+
   case class PartitionsAssigned(clientId: ClientId, group: Group, partitions: Set[TopicPartition]) extends ConsumerMetric
+
   case class PartitionsRevoked(clientId: ClientId, group: Group, partitions: Set[TopicPartition]) extends ConsumerMetric
+
   case class SubscribeFailed(clientId: ClientId, group: Group, error: Throwable) extends ConsumerMetric
+
   case class PollingFailed(clientId: ClientId, group: Group, error: Throwable) extends ConsumerMetric
-  case class CommitFailed(clientId: ClientId, group: Group, error: Throwable) extends ConsumerMetric
-  case class PausePartitionsFailed(clientId: ClientId, group: Group, error: IllegalStateException) extends ConsumerMetric
-  case class ResumePartitionsFailed(clientId: ClientId, group: Group, error: IllegalStateException) extends ConsumerMetric
-  case class SeekToOffsetFailed(clientId: ClientId, group: Group, error: IllegalStateException) extends ConsumerMetric
+
+  case class CommitFailed(clientId: ClientId, group: Group, error: Throwable, offsets: Map[TopicPartition, Offset]) extends ConsumerMetric
+
+  case class PausePartitionsFailed(clientId: ClientId, group: Group, error: IllegalStateException, partitions: Set[TopicPartition]) extends ConsumerMetric
+
+  case class ResumePartitionsFailed(clientId: ClientId, group: Group, error: IllegalStateException, partitions: Set[TopicPartition]) extends ConsumerMetric
+
+  case class SeekToOffsetFailed(clientId: ClientId, group: Group, error: IllegalStateException, partition: TopicPartition, offset: Offset) extends ConsumerMetric
+
+  case class PolledRecords(clientId: ClientId, group: Group, records: Consumer.Records) extends ConsumerMetric
+
 }
