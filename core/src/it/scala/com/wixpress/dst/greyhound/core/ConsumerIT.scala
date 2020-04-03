c1184642c3b3b5aea2b10af832efcd550c25b842
Natan Silnitsky <natans@wix.com>
2020-04-03 09:47:01 +0300

    greyhound ng: split consumerIT to several ITs in order to pass ciruss build #automerge (#9716)
    GitOrigin-RevId: fc1b3c3d5e3f5dad289ea264c3c53c49b9858af6

--- core/src/it/scala/com/wixpress/dst/greyhound/core/ConsumerIT.scala
+++ core/src/it/scala/com/wixpress/dst/greyhound/core/ConsumerIT.scala
@@ -19,38 +19,17 @@ import zio.random.Random
 class ConsumerIT extends BaseTest[Env] {
   sequential
 
-  override def env: UManaged[Env] =
-    Managed.succeed {
-      new GreyhoundMetric.Live
-        with Blocking.Live
-        with Console.Live
-        with Clock.Live
-        with Random.Live
-    }
+  override def env: UManaged[Env] = ConsumerIT.ManagedEnv
 
-  val resources = for {
-    kafka <- ManagedKafka.make(ManagedKafkaConfig.Default)
-    producer <- Producer.make(ProducerConfig(kafka.bootstrapServers))
-  } yield (kafka, ReportingProducer(producer))
+  val resources = testResources()
 
   val tests = resources.use {
     case (kafka, producer) =>
-      def randomTopic(partitions: Int = ConsumerIT.partitions) = for {
-        topic <- randomId.map(id => s"topic-$id")
-        _ <- kafka.createTopic(TopicConfig(topic, partitions, 1, delete))
-      } yield topic
-
-      def verifyGroupCommitted(topic: Topic, group: Group, partitions: Int) = for {
-        latch <- CountDownLatch.make(partitions)
-        handler = RecordHandler(topic)((_: ConsumerRecord[Chunk[Byte], Chunk[Byte]]) => latch.countDown)
-        _ <- ParallelConsumer.makeFrom(kafka.bootstrapServers, ((OffsetReset.Latest, group) -> handler)).use_ {
-          ZIO.foreachPar_(0 until partitions) { partition =>
-            producer.produce(ProducerRecord(topic, Chunk.empty, partition = Some(partition)))
-          } *> latch.await
-        }
-      } yield ()
+      implicit val _kafka: ManagedKafka = kafka
 
       val simpleTest = for {
+        _ <- console.putStrLn(">>>> starting test: simpleTest")
+
         topic <- randomTopic()
         group <- randomGroup
 
@@ -68,28 +47,9 @@ class ConsumerIT extends BaseTest[Env] {
         message must (beRecordWithKey("foo") and beRecordWithValue("bar"))
       }
 
-      val combinedHandlersTest = for {
-        topic1 <- randomTopic()
-        topic2 <- randomTopic()
-        group <- randomGroup
-
-        records1 <- Queue.unbounded[ConsumerRecord[String, String]]
-        records2 <- Queue.unbounded[ConsumerRecord[Int, Int]]
-        handler1 = RecordHandler(topic1)(records1.offer(_: ConsumerRecord[String, String]))
-        handler2 = RecordHandler(topic2)(records2.offer(_: ConsumerRecord[Int, Int]))
-        handler = handler1.withDeserializers(StringSerde, StringSerde) combine handler2.withDeserializers(IntSerde, IntSerde)
-
-        (record1, record2) <- ParallelConsumer.makeFrom(kafka.bootstrapServers, ((OffsetReset.Latest, group) -> handler.ignore)).use_ {
-          producer.produce(ProducerRecord(topic1, "bar", Some("foo")), StringSerde, StringSerde) *>
-            producer.produce(ProducerRecord(topic2, 2, Some(1)), IntSerde, IntSerde) *>
-            (records1.take zip records2.take)
-        }
-      } yield "consume messages from combined handlers" in {
-        (record1 must (beRecordWithKey("foo") and beRecordWithValue("bar"))) and
-          (record2 must (beRecordWithKey(1) and beRecordWithValue(2)))
-      }
-
       val throttlingTest = for {
+        _ <- console.putStrLn(">>>> starting test: throttlingTest")
+
         topic <- randomTopic(partitions = 2)
         group <- randomGroup
 
@@ -122,6 +82,8 @@ class ConsumerIT extends BaseTest[Env] {
       } yield test
 
       val pauseResumeTest = for {
+        _ <- console.putStrLn(">>>> starting test: pauseResumeTest")
+
         topic <- randomTopic()
         group <- randomGroup
 
@@ -151,6 +113,7 @@ class ConsumerIT extends BaseTest[Env] {
       } yield test
 
       val gracefulShutdownTest = for {
+        _ <- console.putStrLn(">>>> starting test: gracefulShutdownTest")
         topic <- randomTopic()
         group <- randomGroup
 
@@ -172,127 +135,8 @@ class ConsumerIT extends BaseTest[Env] {
         handled must equalTo(1)
       }
 
-      val commitOnRebalanceTest = for {
-        topic <- randomTopic()
-        group <- randomGroup
-
-        allMessages = 400
-        someMessages = 100
-        produce = producer.produce(ProducerRecord(topic, Chunk.empty))
-
-        invocations <- Ref.make(0)
-        handledAll <- CountDownLatch.make(allMessages)
-        handledSome <- CountDownLatch.make(someMessages)
-        handler = RecordHandler(topic) { _: ConsumerRecord[Chunk[Byte], Chunk[Byte]] =>
-          invocations.update(_ + 1) *>
-            handledSome.countDown *>
-            handledAll.countDown
-        }
-        consumer = ParallelConsumer.makeFrom(kafka.bootstrapServers, ((OffsetReset.Latest, group) -> handler))
-
-        startProducing1 <- Promise.make[Nothing, Unit]
-        consumer1 <- consumer.use_(startProducing1.succeed(()) *> handledAll.await).fork
-        _ <- startProducing1.await *> ZIO.foreachPar(0 until someMessages)(_ => produce)
-
-        _ <- handledSome.await
-        startProducing2 <- Promise.make[Nothing, Unit]
-        consumer2 <- consumer.use_(startProducing2.succeed(()) *> handledAll.await).fork
-        _ <- startProducing2.await *> ZIO.foreachPar(someMessages until allMessages)(_ => produce)
-
-        _ <- consumer1.join
-        _ <- consumer2.join
-        allInvocations <- invocations.get
-      } yield "don't reprocess messages after rebalance" in {
-        allInvocations must equalTo(allMessages)
-      }
-
-      val gracefulRebalanceTest = for {
-        group <- randomGroup
-
-        partitions = 2
-        messagesPerPartition = 2
-        topic <- randomTopic(partitions = partitions)
-        _ <- verifyGroupCommitted(topic, group, partitions)
-
-        latch <- CountDownLatch.make(messagesPerPartition * partitions)
-        blocker <- Promise.make[Nothing, Unit]
-        semaphore <- Semaphore.make(1)
-        handler1 = RecordHandler(topic) { _: ConsumerRecord[Chunk[Byte], Chunk[Byte]] =>
-          semaphore.withPermit(blocker.await *> latch.countDown)
-        }
-
-        startProducing <- Promise.make[Nothing, Unit]
-        config1 = ParallelConsumerConfig(
-          bootstrapServers = kafka.bootstrapServers,
-          clientId = "client-1",
-          eventLoopConfig = EventLoopConfig.Default.copy(
-            rebalanceListener = new RebalanceListener[Any] {
-              override def onPartitionsRevoked(partitions: Set[TopicPartition]): UIO[Any] =
-                ZIO.when(partitions.nonEmpty)(blocker.succeed(()))
-
-              override def onPartitionsAssigned(partitions: Set[TopicPartition]): UIO[Any] =
-                ZIO.unit
-            }))
-        consumer1 <- ParallelConsumer.make(config1, Map((OffsetReset.Latest, group) -> handler1)).use_ {
-          startProducing.succeed(()) *> latch.await
-        }.fork
-
-        _ <- startProducing.await
-        _ <- ZIO.foreachPar(0 until messagesPerPartition) { _ =>
-          producer.produce(ProducerRecord(topic, Chunk.empty, partition = Some(0))) zipPar
-            producer.produce(ProducerRecord(topic, Chunk.empty, partition = Some(1)))
-        }
-
-        handler2 = RecordHandler(topic) { _: ConsumerRecord[Chunk[Byte], Chunk[Byte]] =>
-          latch.countDown
-        }
-        config2 = ParallelConsumerConfig(kafka.bootstrapServers, "client-2")
-        consumer2 <- ParallelConsumer.make(config2, Map((OffsetReset.Latest, group) -> handler2)).use_ {
-          latch.await
-        }.fork
-
-        _ <- consumer1.join
-        _ <- consumer2.join
-        handled <- latch.count
-      } yield "drain queues and finish executing tasks on rebalance" in {
-        handled must equalTo(0)
-      }
-
-      val retryTest = for {
-        topic <- randomTopic()
-        group <- randomGroup
-        _ <- kafka.createTopic(TopicConfig(s"$topic-$group-retry-0", partitions, 1, delete))
-        _ <- kafka.createTopic(TopicConfig(s"$topic-$group-retry-1", partitions, 1, delete))
-        _ <- kafka.createTopic(TopicConfig(s"$topic-$group-retry-2", partitions, 1, delete))
-
-        invocations <- Ref.make(0)
-        done <- Promise.make[Nothing, Unit]
-        retryPolicy = RetryPolicy.default(topic, group, 1.second, 1.seconds, 1.seconds)
-        handler = RecordHandler(topic) { _: ConsumerRecord[String, String] =>
-          invocations.update(_ + 1).flatMap { n =>
-            if (n < 4) {
-              println(s"failling.. $n")
-              ZIO.fail(new RuntimeException("Oops!"))
-            } else {
-              println(s"success!  $n")
-              done.succeed(()) // Succeed on final retry
-            }
-          }
-        }
-        retryHandler = handler
-          .withDeserializers(StringSerde, StringSerde)
-          .withRetries(retryPolicy, producer)
-          .ignore
-
-        success <- ParallelConsumer.makeFrom(kafka.bootstrapServers, ((OffsetReset.Latest, group) -> retryHandler)).use_ {
-          producer.produce(ProducerRecord(topic, "bar", Some("foo")), StringSerde, StringSerde) *>
-            done.await.timeout(1.minute)
-        }
-      } yield "configure a handler with retry policy" in {
-        success must beSome
-      }
-
       val earliestTest = for {
+        _ <- console.putStrLn(">>>> starting test: earliestTest")
         topic <- randomTopic()
         group <- randomGroup
 
@@ -312,15 +156,11 @@ class ConsumerIT extends BaseTest[Env] {
       }
 
       all(
-        retryTest,
         earliestTest,
         simpleTest,
-        combinedHandlersTest,
         throttlingTest,
         pauseResumeTest,
-        gracefulShutdownTest,
-        commitOnRebalanceTest,
-        gracefulRebalanceTest)
+        gracefulShutdownTest)
   }
 
   run(tests)
@@ -329,6 +169,28 @@ class ConsumerIT extends BaseTest[Env] {
 
 object ConsumerIT {
   type Env = GreyhoundMetrics with Blocking with Console with Clock with Random
+
+  def ManagedEnv = {
+    Managed.succeed {
+      new GreyhoundMetric.Live
+        with Blocking.Live
+        with Console.Live
+        with Clock.Live
+        with Random.Live
+    }
+  }
+
+  def testResources() = {
+    for {
+      kafka <- ManagedKafka.make(ManagedKafkaConfig.Default)
+      producer <- Producer.make(ProducerConfig(kafka.bootstrapServers))
+    } yield (kafka, ReportingProducer(producer))
+  }
+
+  def randomTopic(partitions: Int = ConsumerIT.partitions)(implicit kafka: ManagedKafka) = for {
+    topic <- randomId.map(id => s"topic-$id")
+    _ <- kafka.createTopic(TopicConfig(topic, partitions, 1, delete))
+  } yield topic
 
   val clientId = "greyhound-consumers"
   val partitions = 8
