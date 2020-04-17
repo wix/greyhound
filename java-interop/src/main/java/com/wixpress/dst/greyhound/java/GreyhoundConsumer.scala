efd4aa8949e08158d4bdd1106006a4f572223aeb
Noam Berman <noambe@wix.com>
2020-04-17 19:33:41 +0300

    greyhound ng: add error handling  (#9947)
    * greyhound ng: add error handling #automerge
    
    GitOrigin-RevId: 204c654917174a6889a53cd7dca7b55d561e3cfe

--- java-interop/src/main/java/com/wixpress/dst/greyhound/java/GreyhoundConsumer.scala
+++ java-interop/src/main/java/com/wixpress/dst/greyhound/java/GreyhoundConsumer.scala
@@ -1,21 +1,25 @@
 package com.wixpress.dst.greyhound.java
 
-import java.util.concurrent.Executor
+import java.util.concurrent.{CompletableFuture, Executor}
 
 import com.wixpress.dst.greyhound.core.consumer.EventLoop.Handler
-import com.wixpress.dst.greyhound.core.consumer.{ConsumerRecord => CoreConsumerRecord, RecordHandler => CoreRecordHandler}
+import com.wixpress.dst.greyhound.core.consumer.{SerializationError, ConsumerRecord => CoreConsumerRecord, RecordHandler => CoreRecordHandler}
 import com.wixpress.dst.greyhound.core.{Deserializer => CoreDeserializer}
 import com.wixpress.dst.greyhound.future.GreyhoundRuntime.Env
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.Deserializer
 import zio.ZIO
+import Convert.toScala
+
+import scala.concurrent.Promise
 
 class GreyhoundConsumer[K >: AnyRef, V](val topic: String,
                                         val group: String,
                                         val handler: RecordHandler[K, V],
                                         val keyDeserializer: Deserializer[K],
                                         val valueDeserializer: Deserializer[V],
-                                        val offsetReset: OffsetReset) {
+                                        val offsetReset: OffsetReset,
+                                        val errorHandler: ErrorHandler[K, V]) {
 
   def recordHandler(executor: Executor): Handler[Env] = {
     val baseHandler = CoreRecordHandler(topic) { record: CoreConsumerRecord[K, V] =>
@@ -36,12 +40,52 @@ class GreyhoundConsumer[K >: AnyRef, V](val topic: String,
       }
     }
     baseHandler
+      .withErrorHandler { case (t, record) =>
+        ZIO.fromFuture(_ => toScala(errorHandler.onUserException(t, record))).catchAll(_ => ZIO.unit)
+      }
+
       .withDeserializers(CoreDeserializer(keyDeserializer), CoreDeserializer(valueDeserializer))
-      .withErrorHandler { case (error, _) => error match {
-        // TODO handle errors
-        case Left(_) => ZIO.unit
-        case Right(_) => ZIO.unit
+      .withErrorHandler {
+        case (error, record) => error match {
+          case Left(serializationError) =>
+            ZIO.fromFuture(_ => toScala(
+              errorHandler.onSerializationError(serializationError, record.bimap(_.toArray, _.toArray))))
+              .catchAll(_ => ZIO.unit)
+          case _ => ZIO.unit
+        }
       }
+  }
+}
+
+object Convert {
+  def toScala[A](completableFuture: CompletableFuture[A]): scala.concurrent.Future[A] = {
+    val promise = Promise[A]()
+
+    completableFuture.whenComplete((value, exception) => {
+      Option(exception) match {
+        case Some(ex) => promise.tryFailure(ex)
+        case None => promise.trySuccess(value)
       }
+    })
+
+    promise.future
+  }
+}
+
+
+trait ErrorHandler[K, V] {
+  self =>
+  def onUserException(e: Throwable, record: CoreConsumerRecord[K, V]): CompletableFuture[Unit]
+
+  def onSerializationError(e: SerializationError, record: CoreConsumerRecord[Array[Byte], Array[Byte]]): CompletableFuture[Unit]
+}
+
+object ErrorHandler {
+  def NoOp[K, V]: ErrorHandler[K, V] = new ErrorHandler[K, V] {
+    override def onUserException(e: Throwable, record: CoreConsumerRecord[K, V]): CompletableFuture[Unit] =
+      CompletableFuture.completedFuture(())
+
+    override def onSerializationError(e: SerializationError, record: CoreConsumerRecord[Array[Byte], Array[Byte]]): CompletableFuture[Unit] =
+      CompletableFuture.completedFuture(())
   }
 }
