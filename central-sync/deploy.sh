71ca620f65b87b03abb0b24762b027c81ce1de9c
Natan Silnitsky <natans@wix.com>
2020-04-20 23:13:08 +0300

    greyhound-ng: switch to MIT license #automerge (#10008)
    GitOrigin-RevId: 5c4b857a0640d90788ea21abcc5dae10078897b2

---
+++ central-sync/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#make sure you have a secret gpg key: gpg --list-secret-keys
+#Set version in VERSION file
+#export DEPLOY_MAVEN_USERNAME=YYY
+#export DEPLOY_MAVEN_PASSWORD=XXX
+bazel run //data-streams/greyhound/core:deploy-maven  -- release --gpg
+bazel run //data-streams/greyhound/future-interop:deploy-maven  -- release --gpg
+bazel run //data-streams/greyhound/java-interop:deploy-maven  -- release --gpg
