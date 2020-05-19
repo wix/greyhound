e7380a41acc5a824cb45bb225a542dad43720d61
Natan Silnitsky <natans@wix.com>
2020-05-19 12:24:01 +0300

    offset-monitor: add kafka producer+consumer for sharded get-lag. still keeping grpc response for now for compatibility  (#10613)
    * offset-monitor: add kafka producer+consumer for sharded get-lag. still keeping grpc response for now for compatibility #automerge
    
    * fix aggregate core target build by adding macros dependency and plugin
    
    GitOrigin-RevId: 40cb6e827df45de3609ad9ddbbb7b9fd49b7ce94

--- core/src/main/scala/com/wixpress/dst/greyhound/core/metrics/GreyhoundMetric.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/metrics/GreyhoundMetric.scala
@@ -2,6 +2,8 @@ package com.wixpress.dst.greyhound.core.metrics
 
 import org.slf4j.LoggerFactory
 import zio.ZIO
+import zio.macros.annotation.delegate
+import zio.macros.delegate.Mix
 
 trait GreyhoundMetric extends Product with Serializable
 
@@ -16,4 +18,13 @@ object GreyhoundMetric {
   }
 
   object Live extends Live
+
+  implicit class Ops(val self: GreyhoundMetrics) {
+    def combine[R](r: R)(implicit ev: Mix[R, GreyhoundMetrics]): R with GreyhoundMetrics = {
+      class WithGreyhoundMetrics(@delegate r: R) extends GreyhoundMetrics {
+        override val metrics = self.metrics
+      }
+      ev.mix(r, new WithGreyhoundMetrics(r))
+    }
+  }
 }
