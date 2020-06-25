c5a18eb8e5a06226218520a9fb8260e5a10142a0
Noam Berman <noambe@wix.com>
2020-06-25 16:33:13 +0300

    greyhound: more refactor to builders (#11266)
    * greyhound: more refactor to builders #pr
    
    * fix build
    
    * fix build
    
    * fix producer test
    
    GitOrigin-RevId: a75fadca0de9012174b06e445e398516e19d0007

--- core/src/main/scala/com/wixpress/dst/greyhound/core/producer/ReportingProducer.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/producer/ReportingProducer.scala
@@ -4,28 +4,41 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 
 import com.wixpress.dst.greyhound.core.metrics.{GreyhoundMetric, GreyhoundMetrics}
 import com.wixpress.dst.greyhound.core.producer.ProducerMetric._
+import com.wixpress.dst.greyhound.core.producer.ReportingProducer.Dependencies
 import zio.clock.Clock
-import zio.{Chunk, ZIO}
+import zio.{Chunk, ULayer, ZIO}
 
 import scala.concurrent.duration.FiniteDuration
 
-case class ReportingProducer[-R](internal: Producer[R])
-  extends Producer[R with GreyhoundMetrics with Clock] {
+case class ReportingProducer(internal: Producer, layers: Dependencies)
+  extends Producer {
 
-  override def produce(record: ProducerRecord[Chunk[Byte], Chunk[Byte]]): ZIO[R with GreyhoundMetrics with Clock, ProducerError, RecordMetadata] =
-    GreyhoundMetrics.report(ProducingRecord(record)) *>
+  override def produce(record: ProducerRecord[Chunk[Byte], Chunk[Byte]]): ZIO[Any, ProducerError, RecordMetadata] =
+    (GreyhoundMetrics.report(ProducingRecord(record)) *>
       internal.produce(record).timed.flatMap {
         case (duration, metadata) =>
           GreyhoundMetrics.report(RecordProduced(metadata, FiniteDuration(duration.toMillis, MILLISECONDS))).as(metadata)
       }.tapError { error =>
         GreyhoundMetrics.report(ProduceFailed(error))
-      }
+      }).provideLayer(layers)
+}
+
+object ReportingProducer {
+  type Dependencies = ULayer[GreyhoundMetrics with Clock]
+
+  def apply(internal: Producer, layers: ULayer[GreyhoundMetrics with Clock] = GreyhoundMetrics.liveLayer ++ Clock.live): ReportingProducer =
+    new ReportingProducer(internal, layers)
 }
 
 sealed trait ProducerMetric extends GreyhoundMetric
 
 object ProducerMetric {
+
   case class ProducingRecord(record: ProducerRecord[Chunk[Byte], Chunk[Byte]]) extends ProducerMetric
+
   case class RecordProduced(metadata: RecordMetadata, duration: FiniteDuration) extends ProducerMetric
+
   case class ProduceFailed(error: ProducerError) extends ProducerMetric
+
 }
+
