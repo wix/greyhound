55f67ba1e2488a551a0884b53938afac7bd2038b
Noam Berman <noambe@wix.com>
2020-06-25 16:33:13 +0300

    greyhound: more refactor to builders (#11266)
    * greyhound: more refactor to builders #pr
    
    * fix build
    
    * fix build
    
    * fix producer test
    
    GitOrigin-RevId: a75fadca0de9012174b06e445e398516e19d0007

--- core/src/test/scala/com/wixpress/dst/greyhound/core/producer/ReportingProducerTest.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/producer/ReportingProducerTest.scala
@@ -6,20 +6,20 @@ import com.wixpress.dst.greyhound.core.producer.ProducerMetric._
 import com.wixpress.dst.greyhound.core.producer.ReportingProducerTest._
 import com.wixpress.dst.greyhound.core.testkit.{BaseTest, FakeProducer, TestMetrics}
 import zio._
-import zio.clock.Clock
 import zio.duration._
-import zio.test.environment.TestClock
+import zio.test.environment.{TestClock, TestEnvironment}
 
 import scala.concurrent.duration.FiniteDuration
 
-class ReportingProducerTest extends BaseTest[Clock with TestClock with TestMetrics] {
+class ReportingProducerTest extends BaseTest[Unit] {
 
-  override def env: UManaged[Clock with TestClock with TestMetrics] = {
+  override def env: UManaged[Unit] = ZManaged.succeed(())
+
+  def testEnv: UManaged[TestEnvironment with TestMetrics] =
     for {
       env <- zio.test.environment.testEnvironment.build
       testMetrics <- TestMetrics.make
     } yield env ++ testMetrics
-  }
 
   "delegate to internal producer" in {
     for {
@@ -30,40 +30,50 @@ class ReportingProducerTest extends BaseTest[Clock with TestClock with TestMetri
     } yield produced must equalTo(record)
   }
 
-  "report metric when producing" in {
+  "report metric when producing" in testEnv.use(deps =>
     for {
-      internal <- FakeProducer.make
-      producer = ReportingProducer(internal)
-      _ <- producer.produce(record)
-      metrics <- TestMetrics.reported
+      fakeProducer <- makeProducer(deps)
+      _ <- fakeProducer.produce(record)
+      metrics <- reportedMetrics(deps)
     } yield metrics must contain(ProducingRecord(record))
-  }
+  )
 
-  "report metric when message is produced successfully" in {
+  "report metric when message is produced successfully" in testEnv.use(deps =>
     for {
       promise <- Promise.make[Nothing, Unit]
       metadata = RecordMetadata(topic, partition, 0)
-      internal = new Producer[Any] {
+      internal = new Producer {
         override def produce(record: ProducerRecord[Chunk[Byte], Chunk[Byte]]): IO[ProducerError, RecordMetadata] =
           promise.await.as(metadata)
       }
-      producer = ReportingProducer(internal)
+      producer = producerFrom(deps, internal)
       fiber <- producer.produce(record).fork
-      _ <- TestClock.adjust(1.second)
+      _ <- adjustTestClock(deps, 1.second)
       _ <- promise.succeed(()) *> fiber.join
-      metrics <- TestMetrics.reported
+      metrics <- reportedMetrics(deps)
     } yield metrics must contain(RecordProduced(metadata, FiniteDuration(1, TimeUnit.SECONDS)))
-  }
+  )
 
-  "report metric when produce fails" in {
+  "report metric when produce fails" in testEnv.use(deps =>
     for {
       internal <- FakeProducer.make
-      producer = ReportingProducer(internal.failing)
+      producer = producerFrom(deps, internal.failing)
       _ <- producer.produce(record).either
-      metrics <- TestMetrics.reported
+      metrics <- reportedMetrics(deps)
     } yield metrics must contain(beAnInstanceOf[ProduceFailed])
-  }
+  )
 
+  private def reportedMetrics(deps: _root_.zio.test.environment.TestEnvironment with TestMetrics) =
+    TestMetrics.reported.provideLayer(ZLayer.succeedMany(deps))
+
+  private def producerFrom(deps: TestEnvironment with TestMetrics, underlying: Producer) =
+    ReportingProducer(underlying, ZLayer.succeedMany(deps))
+
+  private def makeProducer(deps: TestEnvironment with TestMetrics) =
+    FakeProducer.make.map(p => producerFrom(deps, p))
+
+  private def adjustTestClock(deps: TestEnvironment with TestMetrics, by: Duration) =
+    TestClock.adjust(by).provideLayer(ZLayer.succeedMany(deps))
 }
 
 object ReportingProducerTest {
