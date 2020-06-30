86d342e90d795a5f3936c5b0676407bcaf45a427
Natan Silnitsky <natans@wix.com>
2020-06-30 16:58:03 +0300

    greyhound ng: testMetrics with logs #automerge (#11356)
    GitOrigin-RevId: 0341855b25cab4b4a9b42d90f07bc0fb64f0284f

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/TestMetrics.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/TestMetrics.scala
@@ -1,6 +1,7 @@
 package com.wixpress.dst.greyhound.core.testkit
 
 import com.wixpress.dst.greyhound.core.metrics.{GreyhoundMetric, GreyhoundMetrics}
+import org.slf4j.LoggerFactory
 import zio._
 
 object TestMetrics {
@@ -9,14 +10,21 @@ object TestMetrics {
     def reported: UIO[List[GreyhoundMetric]] = queue.takeAll
   }
 
-  def make: UManaged[TestMetrics] =
+  def make: UManaged[TestMetrics] = {
+    val logger = LoggerFactory.getLogger("metrics")
+
+    def reportLive(metric: GreyhoundMetric) = {
+      UIO(logger.info(metric.toString))
+    }
+
     Queue.unbounded[GreyhoundMetric].toManaged_.map { q =>
       val service = new Service {
-        override def report(metric: GreyhoundMetric): UIO[Unit] = q.offer(metric).unit
+        override def report(metric: GreyhoundMetric): UIO[Unit] = reportLive(metric) *> q.offer(metric).unit
         override def queue: Queue[GreyhoundMetric] = q
       }
       Has(service) ++ Has(service: GreyhoundMetrics.Service)
     }
+  }
 
   def queue: URIO[TestMetrics, Queue[GreyhoundMetric]] =
     ZIO.access[TestMetrics](_.get.queue)
