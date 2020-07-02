3275b10b5c094fa4886b3c9222c1759efd87b54f
Natan Silnitsky <natans@wix.com>
2020-07-02 18:14:06 +0300

    greyhound-ng unflake dispatcherTest (#11418)
    * check one test case
    
    * unflake
    
    * bring back test cases
    
    * debug prints
    
    * debug prints
    
    * debug prints
    
    * unflake - higher low-watermark
    
    * just one test
    
    * debug
    
    * debug
    
    * all tests
    
    * remove debugs
    
    GitOrigin-RevId: f550439e57e464dfd679aca3ef630dfacb722cfc

--- core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/DispatcherTest.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/DispatcherTest.scala
@@ -3,9 +3,9 @@ package com.wixpress.dst.greyhound.core.consumer
 import com.wixpress.dst.greyhound.core.Headers
 import com.wixpress.dst.greyhound.core.consumer.Dispatcher.Record
 import com.wixpress.dst.greyhound.core.consumer.DispatcherMetric.RecordHandled
-import com.wixpress.dst.greyhound.core.consumer.DispatcherTest._
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetric
-import com.wixpress.dst.greyhound.core.testkit.{BaseTest, CountDownLatch, TestMetrics}
+import com.wixpress.dst.greyhound.core.testkit.{BaseTest, CountDownLatch, TestMetrics, eventuallyZ}
+import org.specs2.specification.Scope
 import zio.clock.Clock
 import zio.duration._
 import zio.test.environment.TestClock
@@ -19,19 +19,19 @@ class DispatcherTest extends BaseTest[Clock with TestClock with TestMetrics] {
       testMetrics <- TestMetrics.make
     } yield env ++ testMetrics
 
-  "handle submitted records" in {
-    for {
+  "handle submitted records" in new ctx() {
+    run(for {
       promise <- Promise.make[Nothing, Record]
       dispatcher <- Dispatcher.make("group", "clientId", promise.succeed, lowWatermark, highWatermark)
       _ <- dispatcher.submit(record)
       handled <- promise.await
-    } yield handled must equalTo(record)
+    } yield handled must equalTo(record))
   }
 
-  "parallelize handling based on partition" in {
+  "parallelize handling based on partition" in new ctx() {
     val partitions = 8
 
-    for {
+    run(for {
       latch <- CountDownLatch.make(partitions)
       slowHandler = { _: ConsumerRecord[Chunk[Byte], Chunk[Byte]] =>
         clock.sleep(1.second) *> latch.countDown
@@ -42,49 +42,50 @@ class DispatcherTest extends BaseTest[Clock with TestClock with TestMetrics] {
       }
       _ <- TestClock.adjust(1.second)
       _ <- latch.await
-    } yield ok // If execution is not parallel, the latch will not be released
+    } yield ok) // If execution is not parallel, the latch will not be released
   }
 
-  "reject records when high watermark is reached" in {
-    for {
+  "reject records when high watermark is reached" in new ctx() {
+    run(for {
       dispatcher <- Dispatcher.make("group", "clientId", _ => ZIO.never, lowWatermark, highWatermark)
       _ <- dispatcher.submit(record.copy(offset = 0L)) // Will be polled
       _ <- dispatcher.submit(record.copy(offset = 1L))
       _ <- dispatcher.submit(record.copy(offset = 2L))
       _ <- dispatcher.submit(record.copy(offset = 3L))
       _ <- dispatcher.submit(record.copy(offset = 4L))
-      result <- dispatcher.submit(record.copy(offset = 5L)) // Will be dropped
-    } yield result must equalTo(SubmitResult.Rejected)
+      result1 <- dispatcher.submit(record.copy(offset = 5L)) // maybe will be dropped
+      result2 <- dispatcher.submit(record.copy(offset = 6L)) // maybe will be dropped
+    } yield (result1 must equalTo(SubmitResult.Rejected)) or (result2 must equalTo(SubmitResult.Rejected)))
   }
 
-  "resume paused partitions" in {
-    for {
-      queue <- Queue.bounded[Record](1)
-      dispatcher <- Dispatcher.make("group", "clientId", queue.offer, lowWatermark, highWatermark)
-      _ <- ZIO.foreach(0 to (highWatermark + 1)) { offset =>
-        dispatcher.submit(ConsumerRecord[Chunk[Byte], Chunk[Byte]](topic, partition, offset, Headers.Empty, None, Chunk.empty, 0L, 0L, 0L))
-      }
-      _ <- dispatcher.submit(ConsumerRecord[Chunk[Byte], Chunk[Byte]](topic, partition, 6L, Headers.Empty, None, Chunk.empty, 0L, 0L, 0L)) // Will be dropped
-      partitionsToResume1 <- dispatcher.resumeablePartitions(Set(topicPartition))
-      _ <- queue.take *> queue.take
-      _ <- UIO(Thread.sleep(1))
-      partitionsToResume2 <- dispatcher.resumeablePartitions(Set(topicPartition))
-    } yield (partitionsToResume1 must beEmpty) and
-      (partitionsToResume2 must equalTo(Set(TopicPartition(topic, partition))))
+  "resume paused partitions" in new ctx(lowWatermark = 3, highWatermark = 7) {
+    run(
+      for {
+        queue <- Queue.bounded[Record](1)
+        dispatcher <- Dispatcher.make("group", "clientId", (record) =>  queue.offer(record).flatMap(result => UIO(println(s"queue.offer result: ${result}"))), lowWatermark, highWatermark)
+        _ <- ZIO.foreach(0 to (highWatermark + 1)) { offset =>
+          dispatcher.submit(ConsumerRecord[Chunk[Byte], Chunk[Byte]](topic, partition, offset, Headers.Empty, None, Chunk.empty, 0L, 0L, 0L))
+        }
+        _ <- dispatcher.submit(ConsumerRecord[Chunk[Byte], Chunk[Byte]](topic, partition, 6L, Headers.Empty, None, Chunk.empty, 0L, 0L, 0L)) // Will be dropped
+        _ <- eventuallyZ(dispatcher.resumeablePartitions(Set(topicPartition)), (set:Set[TopicPartition]) => set.isEmpty)
+        _ <- ZIO.foreach_(1 to 4 )(_ => queue.take)
+        _ <- eventuallyZ(dispatcher.resumeablePartitions(Set(topicPartition)), (set:Set[TopicPartition]) => set == Set(TopicPartition(topic, partition)))
+      } yield ok
+    )
   }
 
-  "pause handling" in {
-    for {
+  "pause handling" in new ctx() {
+    run(for {
       ref <- Ref.make(0)
       dispatcher <- Dispatcher.make("group", "clientId", _ => ref.update(_ + 1), lowWatermark, highWatermark)
       _ <- dispatcher.pause
       _ <- dispatcher.submit(record) // Will be queued
       invocations <- ref.get
-    } yield invocations must equalTo(0)
+    } yield invocations must equalTo(0))
   }
 
-  "complete executing task before pausing" in {
-    for {
+  "complete executing task before pausing" in new ctx() {
+    run(for {
       ref <- Ref.make(0)
       promise <- Promise.make[Nothing, Unit]
       handler = { _: Record =>
@@ -100,15 +101,15 @@ class DispatcherTest extends BaseTest[Clock with TestClock with TestMetrics] {
       _ <- TestClock.adjust(1.second)
       _ <- promise.await
       invocations <- ref.get
-    } yield invocations must equalTo(1)
+    } yield invocations must equalTo(1))
   }
 
   private def waitUntilRecordHandled(timeout: Duration)(metrics: Seq[GreyhoundMetric]) =
     ZIO.when(metrics.collect { case r: RecordHandled[_, _] => r }.nonEmpty)(ZIO.fail(TimeoutWaitingForHandledMetric))
       .retry(Schedule.duration(timeout))
 
-  "resume handling" in {
-    for {
+  "resume handling" in new ctx() {
+    run(for {
       ref <- Ref.make(0)
       promise <- Promise.make[Nothing, Unit]
       handler = { _: ConsumerRecord[Chunk[Byte], Chunk[Byte]] =>
@@ -120,19 +121,16 @@ class DispatcherTest extends BaseTest[Clock with TestClock with TestMetrics] {
       _ <- dispatcher.resume
       _ <- promise.await
       invocations <- ref.get
-    } yield invocations must equalTo(1)
+    } yield invocations must equalTo(1))
   }
 
-}
+  abstract class ctx(val lowWatermark: Int = 2, val highWatermark: Int = 4) extends Scope {
+    val topic = "topic"
+    val partition = 0
+    val topicPartition = TopicPartition(topic, partition)
+    val record = ConsumerRecord[Chunk[Byte], Chunk[Byte]](topic, partition, 0L, Headers.Empty, None, Chunk.empty, 0L, 0L, 0L)
+  }
 
-object DispatcherTest {
-  val lowWatermark = 2
-  val highWatermark = 4
-
-  val topic = "topic"
-  val partition = 0
-  val topicPartition = TopicPartition(topic, partition)
-  val record = ConsumerRecord[Chunk[Byte], Chunk[Byte]](topic, partition, 0L, Headers.Empty, None, Chunk.empty, 0L, 0L, 0L)
 }
 
 case object TimeoutWaitingForHandledMetric extends GreyhoundMetric
