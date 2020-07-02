212035bed9725e979a4a771a2a9dd3ef8f9c74f1
Natan Silnitsky <natans@wix.com>
2020-07-02 18:14:06 +0300

    greyhound-ng unflake dispatcherTest (#11418)
    * check one test case
    
    * unflake
    
    * bring back test cases
    
    * debug prints
    
    * debug prints
    
    * debug prints
    
    * unflake - higher low-watermark
    
    * just one test
    
    * debug
    
    * debug
    
    * all tests
    
    * remove debugs
    
    GitOrigin-RevId: f550439e57e464dfd679aca3ef630dfacb722cfc

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
@@ -1,8 +1,14 @@
 package com.wixpress.dst.greyhound.core
 
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
-import zio.Has
+import zio.clock.Clock
+import zio.{Has, RIO, Schedule, UIO}
+import zio.duration._
 
 package object testkit {
   type TestMetrics = Has[TestMetrics.Service] with GreyhoundMetrics
+
+  def eventuallyZ[R <: Has[_], T](f: RIO[R, T], predicate: T => Boolean) = {
+    ((UIO(println("f")) *> f).repeat(Schedule.spaced(100.milliseconds) && Schedule.doUntil(predicate)).timeoutFail(new RuntimeException("predicate failed"))(4.seconds)).provideSomeLayer[R](Clock.live)
+  }
 }
