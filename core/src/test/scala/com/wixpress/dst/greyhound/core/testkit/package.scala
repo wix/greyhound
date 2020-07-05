617b10b2e10afdaca41a5a7f1f5dd3181666c965
Noam Berman <noambe@wix.com>
2020-07-05 22:37:01 +0300

    greyhound-ng: allow consumer override to jackson deserilization (can take any Array[Byte] now, so it can process events that didnt originate in greyhound) #automerge (#11470)
    GitOrigin-RevId: 47d9280345ea57f3871da3b6f59e0d7a67360b72

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
@@ -1,14 +1,16 @@
 package com.wixpress.dst.greyhound.core
 
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
+import zio.Schedule.{doUntil, spaced}
 import zio.clock.Clock
-import zio.{Has, RIO, Schedule, UIO}
 import zio.duration._
+import zio.{Has, RIO, ZIO}
 
 package object testkit {
   type TestMetrics = Has[TestMetrics.Service] with GreyhoundMetrics
 
-  def eventuallyZ[R <: Has[_], T](f: RIO[R, T], predicate: T => Boolean) = {
-    ((UIO(println("f")) *> f).repeat(Schedule.spaced(100.milliseconds) && Schedule.doUntil(predicate)).timeoutFail(new RuntimeException("predicate failed"))(4.seconds)).provideSomeLayer[R](Clock.live)
-  }
+  def eventuallyZ[R <: Has[_], T](f: RIO[R, T], predicate: T => Boolean): ZIO[R, Throwable, (Partition, T)] =
+    f.repeat(spaced(100.millis) && doUntil(predicate))
+      .timeoutFail(new RuntimeException("eventuallyZ predicate failed"))(4.seconds)
+      .provideSomeLayer[R](Clock.live)
 }
