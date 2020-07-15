2c3085704566a25be4d8300fcdf2bcdb5fbf4543
Noam Berman <noambe@wix.com>
2020-07-15 16:19:38 +0300

    greyhound-ng: resilient producer WIP (#11620)
    not to be used yet.
    
    GitOrigin-RevId: e5ee84b124d6da7b94aa7fa4232a755b47bfb9a7

---
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/producer/buffered/buffers/LocalBuffer.scala
@@ -0,0 +1,35 @@
+package com.wixpress.dst.greyhound.core.producer.buffered.buffers
+
+import com.wixpress.dst.greyhound.core.producer.buffered.buffers.buffers.PersistedMessageId
+import com.wixpress.dst.greyhound.core.{Headers, Partition, Topic}
+import zio.clock.Clock
+import zio.duration.Duration
+import zio.{Chunk, IO, ZIO}
+
+trait LocalBuffer {
+  def enqueue(message: PersistedMessage): ZIO[Clock, LocalBufferError, PersistedMessageId]
+
+  def take(upTo: Int): ZIO[Clock, LocalBufferError, Seq[PersistedMessage]]
+
+  def delete(messageId: PersistedMessageId): IO[LocalBufferError, Boolean]
+}
+
+case class PersistedMessage(id: PersistedMessageId, target: SerializableTarget, encodedMsg: EncodedMessage, submitted: Long = 0L) {
+  def topic: Topic = target.topic
+}
+
+case class EncodedMessage(value: Chunk[Byte], headers: Headers)
+
+case class LocalBufferError(cause: Throwable)
+
+case class LocalBufferFull(maxMessages: Long) extends RuntimeException(s"Local buffer has exceeded capacity. Max # of unsent messages is $maxMessages.")
+
+case class LocalBufferProducerConfig(maxConcurrency: Int, maxMessagesOnDisk: Long, giveUpAfter: Duration) {
+  def withMaxConcurrency(m: Int): LocalBufferProducerConfig = copy(maxConcurrency = m)
+
+  def withMaxMessagesOnDisk(m: Int): LocalBufferProducerConfig = copy(maxMessagesOnDisk = m)
+
+  def withGiveUpAfter(d: Duration): LocalBufferProducerConfig = copy(giveUpAfter = d)
+}
+
+case class SerializableTarget(topic: Topic, partition: Option[Partition], key: Option[Chunk[Byte]])
