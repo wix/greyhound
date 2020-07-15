b980a1df4f449e18b6d3abe28039282780c1ec6d
Natan Silnitsky <natans@wix.com>
2020-07-15 19:25:13 +0300

    greyhound ng: fix to only rely on inner-greyhound-ng deps #automerge (#11697)
    GitOrigin-RevId: d84006b6a01ea2dc1187d3c1ba9fbefb6e642148

---
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/TestClockUtils.scala
@@ -0,0 +1,28 @@
+package com.wixpress.dst.greyhound.core.testkit
+
+import java.time.Instant
+
+import zio.ZIO
+import zio.duration.{Duration, _}
+import zio.test.environment.TestClock
+
+object TestClockUtils {
+  def adjustClock(tickDuration: Duration) = {
+    log(s"moved clock ${tickDuration.toMillis}") *>
+      TestClock.adjust(tickDuration) *>
+      waitALittle
+  }
+
+  private def waitALittle = {
+    sleep(200.millis)
+  }
+
+  private def sleep(duration: Duration) = {
+    zio.test.environment.live(zio.clock.sleep(duration))
+  }
+
+  private def log(str: String) = for {
+    fiberId <- ZIO.fiberId
+    _ <- ZIO.effectTotal(println(s"[${Instant.now}][$fiberId] $str"))
+  } yield ()
+}
