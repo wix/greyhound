cc5c07bd9f4a9abd64b01a39269831567a5141cc
Natan Silnitsky <natans@wix.com>
2020-07-19 16:09:03 +0300

    greyhound ng-  app-info actions E2E - fix bugs (#11680)
    * greyhound ng-  app-info actions E2E - fix bugs
    
    * return BlockedMessage in app-info
    
    * fix test
    
    GitOrigin-RevId: b39e35e70ca22708c2b40266938e6674836ec33f

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingState.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingState.scala
@@ -1,5 +1,6 @@
 package com.wixpress.dst.greyhound.core.consumer.retry
 
+import com.wixpress.dst.greyhound.core.{Headers, Offset}
 import com.wixpress.dst.greyhound.core.consumer.domain.{ConsumerRecord, TopicPartition}
 import com.wixpress.dst.greyhound.core.consumer.retry.RetryRecordHandlerMetric.{BlockingFor, _}
 
@@ -8,6 +9,15 @@ sealed trait BlockingState {
 }
 
 object BlockingState {
+  case object Normal extends BlockingState {
+    override def metric[V, K](record: ConsumerRecord[K, V]) = Silent
+  }
+
+  case class Blocked[V, K](key: Option[K], value: V, messageHeaders: Headers, topicPartition: TopicPartition, offset: Offset) extends BlockingState {
+    override def metric[V, K](record: ConsumerRecord[K, V]) =
+      BlockingFor(TopicPartition(record.topic, record.partition), record.offset)
+  }
+
   case object Blocking extends BlockingState {
     override def metric[V, K](record: ConsumerRecord[K, V]) =
       BlockingFor(TopicPartition(record.topic, record.partition), record.offset)
@@ -21,9 +31,10 @@ object BlockingState {
       BlockingIgnoredOnceFor(TopicPartition(record.topic, record.partition), record.offset)
   }
 
-  def shouldBlockFrom(blockingState: BlockingState) = {
+  def shouldBlockFrom[V, K](blockingState: BlockingState) = {
     blockingState match {
       case Blocking => true
+      case _: Blocked[V, K] => true
       case IgnoringAll => false
       case IgnoringOnce => false
     }
