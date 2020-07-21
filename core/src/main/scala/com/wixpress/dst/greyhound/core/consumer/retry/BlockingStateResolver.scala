d923fc898108052598dccd38d93fd10a22ac1548
Natan Silnitsky <natans@wix.com>
2020-07-21 18:21:01 +0300

    greyhound ng app-info blocking/releasing logic bug fix (#11845)
    * greyhound ng app-info blocking/releasing logic bug fix #automerge
    
    * fix test compilation
    
    GitOrigin-RevId: a6881e9a01bc3246729de95363773ea849d35a40

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingStateResolver.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingStateResolver.scala
@@ -1,25 +1,26 @@
 package com.wixpress.dst.greyhound.core.consumer.retry
 
+import com.wixpress.dst.greyhound.core.Topic
 import com.wixpress.dst.greyhound.core.consumer.domain.{ConsumerRecord, TopicPartition}
-import com.wixpress.dst.greyhound.core.consumer.retry.BlockingState.{IgnoringAll, IgnoringOnce, shouldBlockFrom, Blocking => InternalBlocking}
+import com.wixpress.dst.greyhound.core.consumer.retry.BlockingState.{Blocked, IgnoringAll, IgnoringOnce, shouldBlockFrom, Blocking => InternalBlocking}
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics.report
-import zio.{Ref, UIO, URIO, ZIO}
+import zio._
 
 object BlockingStateResolver {
-  def apply(blockingState: Ref[Map[BlockingTarget, BlockingState]]): BlockingStateResolver = {
+  def apply(blockingStateRef: Ref[Map[BlockingTarget, BlockingState]]): BlockingStateResolver = {
     new BlockingStateResolver {
       override def resolve[K, V](record: ConsumerRecord[K, V]): URIO[GreyhoundMetrics, Boolean] = {
         val topicPartition = TopicPartition(record.topic, record.partition)
 
         for {
-          mergedBlockingState <- blockingState.modify { state =>
+          mergedBlockingState <- blockingStateRef.modify { state =>
             val topicPartitionBlockingState = state.getOrElse(TopicPartitionTarget(topicPartition),InternalBlocking)
             val topicBlockingState = state.getOrElse(TopicTarget(record.topic), InternalBlocking)
             val mergedBlockingState = topicBlockingState match {
               case IgnoringAll => IgnoringAll
               case IgnoringOnce => IgnoringOnce
-              case _ => topicPartitionBlockingState
+              case _ => topicPartitionBlockingState // default is InternalBlocking
             }
             val shouldBlock = shouldBlockFrom(mergedBlockingState)
             val isBlockedAlready = mergedBlockingState match {
@@ -38,10 +39,58 @@ object BlockingStateResolver {
           }
         } yield shouldBlock
       }
+
+      override def setBlockingState[R1](command: BlockingStateCommand): RIO[GreyhoundMetrics, Unit] = {
+        def handleIgnoreOnceRequest(topicPartition: TopicPartition) = {
+          blockingStateRef.modify(prevState => {
+            val previouslyBlocked = prevState.get(TopicPartitionTarget(topicPartition)).exists {
+              case InternalBlocking => true
+              case _: Blocked[Chunk[Byte], Chunk[Byte]] => true
+              case _ => false
+            }
+            if (previouslyBlocked)
+              (true, prevState.updated(TopicPartitionTarget(topicPartition), IgnoringOnce))
+            else
+              (false, prevState)
+          }).flatMap(successful =>
+            ZIO.when(!successful)(
+              ZIO.fail(new RuntimeException("Request to IgnoreOnce when message is not blocked"))))
+
+        }
+
+        def updateTopicTargetAndPartitionTargets(topic: Topic, blockingState: BlockingState) = {
+          blockingStateRef.modify(prevState => {
+            val targetsToUpdate = prevState.filter(pair => pair._1 match {
+              case TopicPartitionTarget(topicPartition) => topicPartition.topic == topic
+              case _ => false
+            })
+
+            ((),prevState.updated(TopicTarget(topic), blockingState) ++ targetsToUpdate.mapValues(_ => blockingState))
+          })
+        }
+
+        command match {
+          case IgnoreOnceFor(topicPartition: TopicPartition)  => handleIgnoreOnceRequest(topicPartition)
+          case IgnoreAllFor(topicPartition: TopicPartition)   => blockingStateRef.update(_.updated(TopicPartitionTarget(topicPartition), IgnoringAll))
+          case BlockErrorsFor(topicPartition: TopicPartition) => blockingStateRef.update(_.updated(TopicPartitionTarget(topicPartition), InternalBlocking))
+          case IgnoreAll(topic: Topic)                        => updateTopicTargetAndPartitionTargets(topic, IgnoringAll)
+          case BlockErrors(topic: Topic)                      => updateTopicTargetAndPartitionTargets(topic, InternalBlocking)
+          case _                                              => ZIO.fail(new RuntimeException(s"unfamiliar BlockingStateCommand: $command"))
+        }
+      }
     }
   }
 }
 
 trait BlockingStateResolver {
   def resolve[K, V](record: ConsumerRecord[K, V]): URIO[GreyhoundMetrics, Boolean]
+  def setBlockingState[R1](command: BlockingStateCommand): RIO[GreyhoundMetrics, Unit]
 }
+
+sealed trait BlockingStateCommand
+
+case class IgnoreOnceFor(topicPartition: TopicPartition) extends BlockingStateCommand
+case class IgnoreAllFor(topicPartition: TopicPartition) extends BlockingStateCommand
+case class BlockErrorsFor(topicPartition: TopicPartition) extends BlockingStateCommand
+case class IgnoreAll(topic: Topic) extends BlockingStateCommand
+case class BlockErrors(topic: Topic) extends BlockingStateCommand
--- core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingStateResolverTest.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingStateResolverTest.scala
@@ -8,7 +8,7 @@ import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.core.testkit.{BaseTest, TestMetrics}
 import org.specs2.specification.core.Fragment
 import zio.test.environment.TestEnvironment
-import zio.{Ref, test}
+import zio.{Ref, UIO, test}
 
 class BlockingStateResolverTest  extends BaseTest[TestEnvironment with GreyhoundMetrics] {
 
@@ -138,6 +138,61 @@ class BlockingStateResolverTest  extends BaseTest[TestEnvironment with Greyhound
           shouldBlock <- resolver.resolve(ConsumerRecord(topic, partition, offset, Headers.Empty, Some(key), value, 0L, 0L, 0L))
         } yield shouldBlock === expectedShouldBlock
       }
+    }
+
+    "when setting blocking state for topicTarget, also set it to related topicPartitions targets transitively, but not to others" in {
+      for {
+        topic <- randomTopicName
+        tpartition = TopicPartition(topic, partition)
+        anotherTopic <- randomTopicName
+        anotherTPartition =  TopicPartition(anotherTopic, partition)
+        blockingState <- Ref.make[Map[BlockingTarget, BlockingState]](Map(
+          TopicPartitionTarget(tpartition) -> IgnoringAll,
+          TopicPartitionTarget(anotherTPartition) -> IgnoringOnce))
+        key = "some-key"
+        value = Foo("some-value")
+        headers = Headers.from("header-key" -> "header-value")
+
+        resolver = BlockingStateResolver(blockingState)
+
+        record = ConsumerRecord(topic, partition, offset, headers, Some(key), value, 0L, 0L, 0L)
+        record2 = ConsumerRecord(anotherTopic, partition, offset, headers, Some(key), value, 0L, 0L, 0L)
+        shouldBlockBefore <- resolver.resolve(record)
+        shouldBlockBefore2 <- resolver.resolve(record2)
+        _ <- resolver.setBlockingState(BlockErrors(topic))
+        shouldBlockAfter <- resolver.resolve(record)
+        shouldBlockAfter2 <- resolver.resolve(record2)
+
+        updatedStateMap <- blockingState.get
+        updatedState = updatedStateMap(TopicPartitionTarget(tpartition))
+        updatedState2 = updatedStateMap(TopicPartitionTarget(anotherTPartition))
+      } yield (shouldBlockBefore aka "shouldBlockBefore" mustEqual(false)) and
+        (shouldBlockAfter aka "shouldBlockAfter" mustEqual(true)) and
+        (shouldBlockBefore2 aka "shouldBlockBefore2" mustEqual(false)) and
+        (shouldBlockAfter2 aka "shouldBlockAfter2" mustEqual(false)) and
+        (updatedState === Blocked(Some(key), value, headers, tpartition, offset)) and
+        (updatedState2 === IgnoringOnce)
+    }
+
+    "fail ignoring once when the previous state was not blocked" in {
+      for {
+        topic <- randomTopicName
+        tpartition = TopicPartition(topic, partition)
+        anotherTopic <- randomTopicName
+        anotherTPartition =  TopicPartition(anotherTopic, partition)
+        blockingState <- Ref.make[Map[BlockingTarget, BlockingState]](Map(TopicPartitionTarget(tpartition) -> InternalBlocking))
+
+        resolver = BlockingStateResolver(blockingState)
+        failed1 <- resolver.setBlockingState(IgnoreOnceFor(tpartition)).as(false).catchAll(_ => UIO(true))
+        failed2 <- resolver.setBlockingState(IgnoreOnceFor(anotherTPartition)).as(false).catchAll(_ => UIO(true))
+
+        updatedStateMap <- blockingState.get
+        updatedState1 = updatedStateMap(TopicPartitionTarget(tpartition))
+        updatedState2 = updatedStateMap.getOrElse(TopicPartitionTarget(anotherTPartition), InternalBlocking)
+      } yield failed1 === false and
+        updatedState1 === IgnoringOnce and
+        failed2 === true and
+        updatedState2 === InternalBlocking
     }
   }
 
