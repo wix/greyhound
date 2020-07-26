71fb04b50502fff3dc70a6996d492ee0f0a678f3
Natan Silnitsky <natans@wix.com>
2020-07-26 17:24:51 +0300

    greyhound-ng add retryAttempt header (#11874)
    * greyhound-ng add retryAttempt header #pr
    
    * add test #automerge
    
    GitOrigin-RevId: b2a4b3803005888a4716b07340feb207cd05bb9c

---
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/Maker.scala
@@ -0,0 +1,33 @@
+package com.wixpress.dst.greyhound.core.testkit
+
+import com.wixpress.dst.greyhound.core.Headers
+import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerRecord
+import zio.random.{nextBytes, nextIntBounded}
+import zio.{ZIO, random}
+
+object Maker {
+  val bytes = nextIntBounded(9).flatMap(size => nextBytes(size + 1))
+
+  def randomAlphaChar = {
+    val low = 'A'.toInt
+    val high = 'z'.toInt + 1
+    random.nextIntBetween(low, high).map(_.toChar)
+  }
+
+  def randomStr = ZIO.collectAll(List.fill(6)(randomAlphaChar)).map(_.mkString)
+
+  def randomTopicName = randomStr.map(suffix => s"some-topic-$suffix")
+
+  val partition = 0
+  val offset = 0
+  val key = s"some-key-$randomStr"
+  val value = randomStr
+
+  val abytesRecord = for {
+    topic <- randomTopicName
+    key <- bytes
+    value <- bytes
+  } yield new ConsumerRecord(
+    topic, partition, offset, Headers.Empty, Some(key),
+    value, 0L, 0L, 0L)
+}
