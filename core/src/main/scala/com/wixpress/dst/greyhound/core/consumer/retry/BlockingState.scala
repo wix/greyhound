41f4b98783852991091d51c097712bd12acb8f17
Noam Berman <noambe@wix.com>
2020-08-30 14:08:02 +0300

    greyhound-ng: fix blocking partitions gauge; add greyhound version (201) gauge for consumer #automerge #skipreview (#12744)
    GitOrigin-RevId: 46cac9f05597c731d85c7a55d70520baf99046e5

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingState.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/retry/BlockingState.scala
@@ -1,38 +1,39 @@
 package com.wixpress.dst.greyhound.core.consumer.retry
 
-import com.wixpress.dst.greyhound.core.{Headers, Offset}
 import com.wixpress.dst.greyhound.core.consumer.domain.{ConsumerRecord, TopicPartition}
-import com.wixpress.dst.greyhound.core.consumer.retry.RetryRecordHandlerMetric.{BlockingFor, _}
+import com.wixpress.dst.greyhound.core.consumer.retry.RetryRecordHandlerMetric.{BlockingFor, BlockingIgnoredForAllFor, BlockingIgnoredOnceFor}
+import com.wixpress.dst.greyhound.core.{Headers, Offset}
 
 sealed trait BlockingState {
-  def metric[V, K](record: ConsumerRecord[K, V]): RetryRecordHandlerMetric
+  def metric[K, V](record: ConsumerRecord[K, V]): RetryRecordHandlerMetric
 }
 
 object BlockingState {
-  case class Blocked[V, K](key: Option[K], value: V, messageHeaders: Headers, topicPartition: TopicPartition, offset: Offset) extends BlockingState {
-    override def metric[V, K](record: ConsumerRecord[K, V]) =
+  case class Blocked[K, V](key: Option[K], value: V, messageHeaders: Headers, topicPartition: TopicPartition, offset: Offset) extends BlockingState {
+    override def metric[K, V](record: ConsumerRecord[K, V]): BlockingFor =
       BlockingFor(TopicPartition(record.topic, record.partition), record.offset)
   }
 
   case object Blocking extends BlockingState {
-    override def metric[V, K](record: ConsumerRecord[K, V]) =
+    override def metric[K, V](record: ConsumerRecord[K, V]): BlockingFor =
       BlockingFor(TopicPartition(record.topic, record.partition), record.offset)
   }
+
   case object IgnoringAll extends BlockingState {
-    override def metric[V, K](record: ConsumerRecord[K, V]): RetryRecordHandlerMetric =
+    override def metric[K, V](record: ConsumerRecord[K, V]): RetryRecordHandlerMetric =
       BlockingIgnoredForAllFor(TopicPartition(record.topic, record.partition), record.offset)
   }
+
   case object IgnoringOnce extends BlockingState {
-    override def metric[V, K](record: ConsumerRecord[K, V]): RetryRecordHandlerMetric =
+    override def metric[K, V](record: ConsumerRecord[K, V]): RetryRecordHandlerMetric =
       BlockingIgnoredOnceFor(TopicPartition(record.topic, record.partition), record.offset)
   }
 
-  def shouldBlockFrom[V, K](blockingState: BlockingState) = {
+  def shouldBlockFrom(blockingState: BlockingState): Boolean =
     blockingState match {
       case Blocking => true
-      case _: Blocked[V, K] => true
+      case _: Blocked[_, _ ] => true
       case IgnoringAll => false
       case IgnoringOnce => false
     }
-  }
 }
