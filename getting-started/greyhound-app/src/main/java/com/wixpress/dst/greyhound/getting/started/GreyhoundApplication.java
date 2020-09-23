a8ecb97834231e2d0d01a2ef0ab690b7cad5199d
Natan Silnitsky <natans@wix.com>
2020-09-23 11:39:23 +0300

    Greyhound Java App now uses new Greyhound Java Interop API with maxParallelism

--- getting-started/greyhound-app/src/main/java/com/wixpress/dst/greyhound/getting/started/GreyhoundApplication.java
+++ getting-started/greyhound-app/src/main/java/com/wixpress/dst/greyhound/getting/started/GreyhoundApplication.java
@@ -5,7 +5,6 @@ import com.wixpress.dst.greyhound.core.TopicConfig;
 import com.wixpress.dst.greyhound.core.admin.AdminClientConfig;
 import com.wixpress.dst.greyhound.future.AdminClient;
 import com.wixpress.dst.greyhound.java.*;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
@@ -21,9 +20,7 @@ import scala.collection.immutable.HashMap;
 
 import java.time.Duration;
 import java.util.Date;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
 
 import static com.wixpress.dst.greyhound.java.RecordHandlers.aBlockingRecordHandler;
 
@@ -33,7 +30,8 @@ public class GreyhoundApplication implements CommandLineRunner {
 
 	public static final	String BOOT_START_SERVERS = "kafka:29092"; // This is the broker address in the docker
 	public static final	String TOPIC = "greyhound-topic";
-	public static final	String GROUP = "greyhound-group";
+	public static final	String SLOW_GROUP = "greyhound-group-slow";
+	public static final	String FAST_GROUP = "greyhound-group-fast";
 	public static final	int PARTITIONS = 8; // The number of partitions is the number of max parallelism
 
 	private final HashMap<String, String> EMPTY_MAP = new HashMap<>();
@@ -45,8 +43,8 @@ public class GreyhoundApplication implements CommandLineRunner {
 	private int currentNumOfMessages;
 	private long produceStartTime;
 	private long lastConsumeTime;
-	private AtomicInteger counter;
-	private Semaphore semaphore;
+	private AtomicInteger slowCounter;
+	private AtomicInteger fastCounter;
 
 	public GreyhoundApplication() {
 	}
@@ -58,12 +56,12 @@ public class GreyhoundApplication implements CommandLineRunner {
 	}
 
 	@RequestMapping("/produce")
-	public String produce(@RequestParam("numOfMessages") int numOfMessages,
-						  @RequestParam(value = "maxParallelism", defaultValue = "1") int maxParallelism) {
+	public String produce(@RequestParam("numOfMessages") int numOfMessages/*,
+						  @RequestParam(value = "maxParallelism", defaultValue = "1") int maxParallelism*/) {
 		// Save some details so we can measure time it toke to consume all messages and reduce the parallelism
 		currentNumOfMessages = numOfMessages;
-		counter = new AtomicInteger(currentNumOfMessages);
-		semaphore = new Semaphore(maxParallelism);
+		slowCounter = new AtomicInteger(currentNumOfMessages);
+		fastCounter = new AtomicInteger(currentNumOfMessages);
 		produceStartTime = System.currentTimeMillis();
 
 		// Produce the messages
@@ -74,7 +72,9 @@ public class GreyhoundApplication implements CommandLineRunner {
 					new StringSerializer());
 		}
 
-		return "produced " + numOfMessages + " messages at " + new Date(produceStartTime);
+		String message = "produced " + numOfMessages + " messages at " + new Date(produceStartTime);
+		System.out.println(message);
+		return message;
 	}
 
 	/// Application Startup ///
@@ -88,23 +88,10 @@ public class GreyhoundApplication implements CommandLineRunner {
 		GreyhoundConfig config = new GreyhoundConfig(BOOT_START_SERVERS);
 		createTopic(); //Not necessary for topic with default configurations
 		createProducer(config);
-		createConsumer(config);
+		createConsumers(config);
 	}
 
 	/// Greyhound Configurations ///
-	private void createConsumer(GreyhoundConfig config) {
-		consumers = new GreyhoundConsumersBuilder(config)
-				.withConsumer(
-						new GreyhoundConsumer<>(
-								TOPIC,
-								GROUP,
-								aBlockingRecordHandler(getConsumer()),
-								new IntegerDeserializer(),
-								new StringDeserializer(),
-								OffsetReset.Latest,
-								ErrorHandler.NoOp())).build();
-	}
-
 	private void createProducer(GreyhoundConfig config) {
 		producer = new GreyhoundProducerBuilder(config).build();
 	}
@@ -119,24 +106,50 @@ public class GreyhoundApplication implements CommandLineRunner {
 				EMPTY_MAP)).isCompleted();
 	}
 
-	private Consumer<ConsumerRecord<Integer, String>> getConsumer() {
-		return record -> {
-			try {
-				semaphore.acquire(); // Greyhound is max parallelism by default. We use a semaphore to decrease the parallelism.
-				int count = counter.decrementAndGet();
-				Thread.sleep(5); // This is used to simulate work done in the consumer to show the difference between max parallelism & lower
-				System.out.println("Consumed message \"" + record.value() + "\" " + count + " messages remains");
-				if (count == 0) {
-					lastConsumeTime = System.currentTimeMillis();
-					System.out.println("----------------------------------------------------------------------");
-					System.out.println("All messages consumed in " + (lastConsumeTime - produceStartTime) + " millis at " + new Date(lastConsumeTime));
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				semaphore.release();
-			}
-		};
+	private void createConsumers(GreyhoundConfig config) {
+		consumers = new GreyhoundConsumersBuilder(config)
+				.withConsumer(
+						aConsumer(SLOW_GROUP, 1))
+				.withConsumer(
+						aConsumer(FAST_GROUP, 8))
+				.build();
+	}
+
+	private GreyhoundConsumer<Integer, String> aConsumer(String group, int maxParallelism) {
+		GreyhoundConsumer<Integer, String> consumer = GreyhoundConsumer.with(
+				TOPIC,
+				group,
+				aBlockingRecordHandler(record -> {
+					if (getCounterBy(group).get() == currentNumOfMessages)
+						System.out.println("started consuming for '" +  group + "'...");
+					int count = getCounterBy(group).decrementAndGet();
+					artificialDelay();
+					if (count == 0) {
+						lastConsumeTime = System.currentTimeMillis();
+						System.out.println("----------------------------------------------------------------------");
+						System.out.println("Consumer with MaxParallelism = " + maxParallelism + " - All messages consumed in "
+								+ (lastConsumeTime - produceStartTime) + " millis at " + new Date(lastConsumeTime));
+					}
+				}),
+				new IntegerDeserializer(),
+				new StringDeserializer())
+				.withMaxParallelism(maxParallelism);
+		return consumer;
+	}
+
+	private AtomicInteger getCounterBy(String group) {
+		if (group.equals(SLOW_GROUP))
+			return slowCounter;
+		else
+			return fastCounter;
+	}
+
+	private void artificialDelay() {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
