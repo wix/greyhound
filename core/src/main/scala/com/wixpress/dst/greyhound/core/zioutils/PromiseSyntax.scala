c3840dc50c44a93b336a820ccebc2f94450ab15e
Dmitry Karlinsky <dmitryka@wix.com>
2020-11-11 11:29:05 +0200

    grehyound-core|PromiseSyntax - use forkDaemon for make promise work across `unsafeRun`s #automerge (#14247)
    GitOrigin-RevId: 6d79beb642112547b546869bdebf074d73259b7b

--- core/src/main/scala/com/wixpress/dst/greyhound/core/zioutils/PromiseSyntax.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/zioutils/PromiseSyntax.scala
@@ -9,7 +9,7 @@ object PromiseSyntax {
       _ <- promise.await
         .flatMap(a => newPromise.complete(ZIO.succeed(f(a))))
         .catchAllCause(h => newPromise.complete(ZIO.halt(h)))
-        .fork
+        .forkDaemon
     } yield newPromise
 
     def unit: UIO[Promise[E, Unit]] = map(_ => ())
