7e7bfedcf190f10dad5c09587f84484cc0ef4384
Natan Silnitsky <natans@wix.com>
2020-11-29 18:34:01 +0200

    [greyhound] additional retry policies for java wrappers (#14672)
    * [greyhound] additional retry policies for java wrappers
    
    * Fix a test
    
    * minor changes
    
    Co-authored-by: Anton Krapivner <antonkra@wix.com>
    GitOrigin-RevId: b5549c6662f488274af6dadf9f2173f440c148c9

--- java-interop/src/main/java/com/wixpress/dst/greyhound/java/RetryConfig.java
+++ java-interop/src/main/java/com/wixpress/dst/greyhound/java/RetryConfig.java
@@ -1,5 +1,8 @@
 package com.wixpress.dst.greyhound.java;
 
+import com.wixpress.dst.greyhound.core.consumer.retry.ZRetryConfig;
+import scala.collection.JavaConverters;
+
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
@@ -17,11 +20,39 @@ public class RetryConfig {
         return new RetryConfig(Collections.emptyList(), nonBlockingBackoffs);
     }
 
+    public static RetryConfig finiteBlockingRetry(List<Duration> blockingBackoffs) {
+        return new RetryConfig(blockingBackoffs, Collections.emptyList());
+    }
+
+    public static RetryConfig blockingFollowedByNonBlockingRetry(List<Duration> blockingBackoffs, List<Duration> nonBlockingBackoffs) {
+        return new RetryConfig(blockingBackoffs, nonBlockingBackoffs);
+    }
+
+    public static RetryConfig exponentialBackoffBlockingRetry(Duration initialInterval,
+                                                              Duration maximalInterval,
+                                                              float backOffMultiplier,
+                                                              boolean infiniteRetryMaxInterval) {
+        return fromCoreRetryConfig(ZRetryConfig.exponentialBackoffBlockingRetry(initialInterval, maximalInterval, backOffMultiplier, infiniteRetryMaxInterval));
+    }
+
+    public static RetryConfig exponentialBackoffBlockingRetry(Duration initialInterval,
+                                                              int maxMultiplications,
+                                                              float backOffMultiplier,
+                                                              boolean infiniteRetryMaxInterval) {
+        return fromCoreRetryConfig(ZRetryConfig.exponentialBackoffBlockingRetry(initialInterval, maxMultiplications, backOffMultiplier, infiniteRetryMaxInterval));
+    }
+
     public List<Duration> blockingBackoffs() {
         return blockingBackoffs;
     }
 
     public List<Duration> nonBlockingBackoffs() {
         return nonBlockingBackoffs;
+    }
+
+    private static RetryConfig fromCoreRetryConfig(com.wixpress.dst.greyhound.core.consumer.retry.RetryConfig coreRetryConfig) {
+        List<Duration> blocking = JavaConverters.seqAsJavaList(coreRetryConfig.blockingBackoffs().apply());
+        List<Duration> nonBlocking = JavaConverters.seqAsJavaList(coreRetryConfig.nonBlockingBackoffs());
+        return new RetryConfig(blocking, nonBlocking);
     }
 }
