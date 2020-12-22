93f398d0592d36e630d288be0891b910d2fd0947
Dmitry Karlinsky <dmitryka@wix.com>
2020-12-22 13:47:05 +0200

    greyhound-ng|consumer: commit missing offsets on assign (#15173)
    * greyhound-ng|consumer: commit missing offsets on assign #pr
    
    * stop reporting exception to NR; longer timeout;
    
    GitOrigin-RevId: 96534e1bbe8126b152dee962e0e90eea1374eb49

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/package.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/package.scala
@@ -1,10 +1,15 @@
 package com.wixpress.dst.greyhound.core
 
-import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerSubscription
+import java.util
+
+import com.wixpress.dst.greyhound.core.consumer.domain.{ConsumerSubscription, TopicPartition}
 import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerSubscription.{TopicPattern, Topics}
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import zio.{RIO, ZIO}
 import zio.blocking.Blocking
+import org.apache.kafka.common.{TopicPartition => KafkaTopicPartition}
+import scala.collection.JavaConverters._
 
 package object consumer {
   def subscribe[R](subscription: ConsumerSubscription, listener: RebalanceListener[R])(consumer: Consumer): RIO[Blocking with GreyhoundMetrics with R, Unit] =
@@ -14,4 +19,13 @@ package object consumer {
       case Topics(topics) =>
         consumer.subscribe(topics, listener)
     }
+
+  def kafkaOffsets(offsets: Map[TopicPartition, Offset]): util.Map[KafkaTopicPartition, OffsetAndMetadata] =
+    offsets.map {
+      case (tp, offset) => tp.asKafka -> new OffsetAndMetadata(offset)
+    }.asJava
+
+  def kafkaPartitions(partitions: Set[TopicPartition]): util.Collection[KafkaTopicPartition] = {
+    partitions.map(_.asKafka).asJavaCollection
+  }
 }
