9354f5d9b34fb31916437a8edf5a4c4fc4a293f8
Dmitry Karlinsky <dmitryka@wix.com>
2020-12-22 13:47:05 +0200

    greyhound-ng|consumer: commit missing offsets on assign (#15173)
    * greyhound-ng|consumer: commit missing offsets on assign #pr
    
    * stop reporting exception to NR; longer timeout;
    
    GitOrigin-RevId: 96534e1bbe8126b152dee962e0e90eea1374eb49

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/domain/TopicPartition.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/domain/TopicPartition.scala
@@ -3,7 +3,9 @@ package com.wixpress.dst.greyhound.core.consumer.domain
 import com.wixpress.dst.greyhound.core.{Partition, Topic}
 import org.apache.kafka.common.{TopicPartition => KafkaTopicPartition}
 
-case class TopicPartition(topic: Topic, partition: Partition)
+case class TopicPartition(topic: Topic, partition: Partition) {
+  def asKafka: KafkaTopicPartition = new KafkaTopicPartition(topic, partition)
+}
 
 object TopicPartition {
   def apply(record: ConsumerRecord[_, _]): TopicPartition =
