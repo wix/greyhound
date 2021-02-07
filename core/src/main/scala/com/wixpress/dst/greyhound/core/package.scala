70c2658df309e2d151e4751a958c5227e68cea7e
Natan Silnitsky <natans@wix.com>
2021-02-07 19:59:02 +0200

    offset-monitor with admin based group offsets (#16418)
    * offset-monitor with admin based group offsets #draftpr
    
    * fix build
    
    * sharding topic listener
    
    * introducing group offsets sharding
    
    * extract GroupsShardsCalculator
    
    * shard UT
    
    GitOrigin-RevId: 8ca788f8c3c02ab19f4500e92ce94b6054d384ec

--- core/src/main/scala/com/wixpress/dst/greyhound/core/package.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/package.scala
@@ -19,4 +19,7 @@ package object core {
   val instantDeserializer: Deserializer[Instant] = longDeserializer.map(Instant.ofEpochMilli)
   val durationDeserializer = longDeserializer.map(Duration(_, java.util.concurrent.TimeUnit.MILLISECONDS))
 
+  case class GroupTopicPartition(group: String, topicPartition: TopicPartition) {
+    override def toString = s"[$group, ${topicPartition.topic}, ${topicPartition.partition}]"
+  }
 }
