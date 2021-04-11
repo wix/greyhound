39246246518adb572f050db5ebaefd37bc3faef8
Natan Silnitsky <natans@wix.com>
2021-04-12 02:02:11 +0300

    replicator - turn decryptor off #automerge (#17999)
    GitOrigin-RevId: d29714129dedbae5724772e8a162323aba6c0562

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/domain/Decryptor.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/domain/Decryptor.scala
@@ -1,7 +1,11 @@
 package com.wixpress.dst.greyhound.core.consumer.domain
 
-import zio.ZIO
+import zio.{UIO, ZIO}
 
 trait Decryptor[R, +E, K, V] {
   def decrypt(record: ConsumerRecord[K, V]): ZIO[R, E, ConsumerRecord[K, V]]
+}
+
+class NoOpDecryptor[R, +E, K, V] extends Decryptor[R, E, K, V]  {
+  def decrypt(record: ConsumerRecord[K, V]): ZIO[R, E, ConsumerRecord[K, V]] = UIO(record)
 }
