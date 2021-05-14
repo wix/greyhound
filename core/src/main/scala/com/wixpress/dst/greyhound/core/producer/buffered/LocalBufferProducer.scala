a9700b79de09336d1234759610ecd1b5871e5b84
Noam Berman <noambe@wix.com>
2021-05-14 16:21:09 +0300

    resilient producer: report fiber failures to NR; scavenger-flusher: add interval logging of running fiber count #skipreview #automerge (#18853)
    GitOrigin-RevId: 84c9542e2aaaa25544e4c980056c9e41f9b0b736

--- core/src/main/scala/com/wixpress/dst/greyhound/core/producer/buffered/LocalBufferProducer.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/producer/buffered/LocalBufferProducer.scala
@@ -237,6 +237,7 @@ object LocalBufferProducerMetric {
 
   case class LocalBufferProducerCaughtError(e: Throwable) extends LocalBufferProducerMetric
 
+  case class LocalBufferProducerInternalFiberDied() extends LocalBufferProducerMetric
 }
 
 
