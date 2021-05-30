9fc88913b4bc5da651c63da1baf092a724cdfacf
Natan Silnitsky <natans@wix.com>
2021-05-30 10:45:07 +0300

    changes for Greyhound 0.2.0 (#19241)
    * Update BUILD.bazel
    
    * Update BUILD.bazel
    
    * Update deploy.sh
    
    GitOrigin-RevId: 18c3de7705822185cd82817617b6b31790b3fbfb

--- central-sync/deploy.sh
+++ central-sync/deploy.sh
@@ -4,9 +4,9 @@
 #Set version in VERSION file
 #export DEPLOY_MAVEN_USERNAME=YYY
 #export DEPLOY_MAVEN_PASSWORD=XXX
-bazel run //data-streams/greyhound/core:deploy-maven  -- release --gpg
-bazel run //data-streams/greyhound/core:deploy-maven-testkit  -- release --gpg
-bazel run //data-streams/greyhound/future-interop:deploy-maven  -- release --gpg
-bazel run //data-streams/greyhound/java-interop:deploy-maven-java-core  -- release --gpg
-bazel run //data-streams/greyhound/java-interop:deploy-maven  -- release --gpg
+bazel run //core:deploy-maven  -- release --gpg
+bazel run //core:deploy-maven-testkit  -- release --gpg
+bazel run //future-interop:deploy-maven  -- release --gpg
+bazel run //java-interop:deploy-maven-java-core  -- release --gpg
+bazel run //java-interop:deploy-maven  -- release --gpg
 #go to https://oss.sonatype.org/#stagingRepositories, close then release
