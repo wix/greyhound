1ec61271ad0afa7d9b0b5722bc02d4615e060bb7
guygo87 <guygo@wix.com>
2022-06-19 20:50:20 +0300

    sidecar produce empty key fix

--- greyhoundsidecar/src/main/scala/greyhound/Produce.scala
+++ greyhoundsidecar/src/main/scala/greyhound/Produce.scala
@@ -4,8 +4,11 @@ import com.wixpress.dst.greyhound.core.producer._
 import com.wixpress.dst.greyhound.core.Serdes
 import com.wixpress.dst.greyhound.sidecar.api.v1.greyhoundsidecar.ProduceRequest
 import com.wixpress.dst.greyhound.testkit.ManagedKafkaConfig
+import zio.ZIO
 
 object Produce {
+
+  val defaultKey: Option[String] = Some("")
 
   def apply(request: ProduceRequest) =
     Producer.make(config).use { producer =>
@@ -13,9 +16,12 @@ object Produce {
         record = ProducerRecord(
           topic = request.topic,
           value = request.payload.getOrElse(""),
-          key = request.target.key),
+          key = request.target.key orElse defaultKey),
         keySerializer = Serdes.StringSerde,
-        valueSerializer = Serdes.StringSerde)
+        valueSerializer = Serdes.StringSerde
+      ).tapBoth(
+          error => ZIO.succeed(println(s"~~~ PRODUCE ~~~ failed producing a msg, error [$error] request [$request]")),
+          response => ZIO.succeed(println(s"~~~ PRODUCE ~~~ produced a msg! response [$response]")))
     }
 
   val bootstrapServer = s"localhost:${ManagedKafkaConfig.Default.kafkaPort}"
