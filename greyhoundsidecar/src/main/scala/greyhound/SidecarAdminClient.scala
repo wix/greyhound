69e6ac987f03f4bcdec8c726a6bb1775c6b15d20
Natan Silnitsky <natans@wix.com>
2022-06-20 08:08:45 +0300

    prints

--- greyhoundsidecar/src/main/scala/greyhound/SidecarAdminClient.scala
+++ greyhoundsidecar/src/main/scala/greyhound/SidecarAdminClient.scala
@@ -4,5 +4,8 @@ import com.wixpress.dst.greyhound.core.admin.{AdminClient, AdminClientConfig}
 
 object SidecarAdminClient {
 
-  def admin(kafkaAddress: String) = AdminClient.make(AdminClientConfig(kafkaAddress))
+  def admin(kafkaAddress: String) = {
+    println(s"~~~ Admin Kafka address is $kafkaAddress")
+    AdminClient.make(AdminClientConfig(kafkaAddress))
+  }
 }
