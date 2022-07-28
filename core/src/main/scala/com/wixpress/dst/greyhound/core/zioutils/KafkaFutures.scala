76ffbfbde2641188f7bd0c2b90cd5466d3a1eed2
Noam Berman <noambe@wix.com>
2022-07-28 17:40:02 +0300

    Zio2 preview - will be reverted immediately (#28367)
    * Revert "Revert "Zio2 preview (#28356)" (#28363)"
    
    This reverts commit 72978a69b86816d12213455b44949dec8a3e407c.
    
    * randomize kafka port
    
    * no cross-repo checks
    
    * undo ports change
    
    * setting (too) flaky datastore sink to "manual" for now
    
    * remove mate rule
    
    * merge master
    
    * dummy commit
    
    GitOrigin-RevId: 12187c1be5e7f891e988e71f2ca36ceaf62e3cc0

--- core/src/main/scala/com/wixpress/dst/greyhound/core/zioutils/KafkaFutures.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/zioutils/KafkaFutures.scala
@@ -1,15 +1,14 @@
 package com.wixpress.dst.greyhound.core.zioutils
 
 import org.apache.kafka.common.KafkaFuture
-import zio.blocking.Blocking
-import zio.{blocking, RIO, ZIO}
+import zio.{Task, Trace, ZIO}
 
 object KafkaFutures {
   implicit class KafkaFutureOps[A](val future: KafkaFuture[A]) {
-    def asZio: RIO[Blocking, A] = {
-      RIO.effectAsyncInterrupt[Blocking, A] { cb =>
+    def asZio (implicit trace: Trace): Task[A] = {
+      ZIO.asyncInterrupt[Any, Throwable, A] { cb =>
         future.whenComplete { (a, e) => cb(if (e == null) ZIO.succeed(a) else ZIO.fail(e)) }
-        Left(blocking.effectBlocking(future.cancel(true)).ignore)
+        Left(ZIO.attemptBlocking(future.cancel(true)).ignore)
       }
     }
   }
