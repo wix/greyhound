5a43768be15f2c8018b30d916424bd5c9712f822
Noam Berman <noambe@wix.com>
2022-07-28 11:20:54 +0300

    Revert "Zio2 preview (#28356)" (#28363)
    This reverts commit 9701068a72b8d713289659661be9c1ecfd4cc0ae.
    
    GitOrigin-RevId: 72978a69b86816d12213455b44949dec8a3e407c

--- future-interop/src/main/scala/com/wixpress/dst/greyhound/future/AdminClient.scala
+++ future-interop/src/main/scala/com/wixpress/dst/greyhound/future/AdminClient.scala
@@ -2,8 +2,9 @@ package com.wixpress.dst.greyhound.future
 
 import com.wixpress.dst.greyhound.core.{Topic, TopicConfig}
 import com.wixpress.dst.greyhound.core.admin.{AdminClient => AdminClientCore, AdminClientConfig, TopicPropertiesResult}
+import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import zio.ZIO
-
+import zio.blocking.Blocking
 
 import scala.concurrent.Future
 
@@ -21,27 +22,32 @@ object AdminClient {
 
   import GreyhoundRuntime.executionContext
 
-  def create(config: AdminClientConfig): AdminClient =
-    new AdminClient {
-      override def createTopics(configs: Set[TopicConfig]): Future[Map[String, Option[Throwable]]] =
-        GreyhoundRuntime.Live.unsafeRunToFuture {
-          ZIO.scoped(AdminClientCore.make(config).flatMap { client => client.createTopics(configs) })
+  def create(config: AdminClientConfig): AdminClient = GreyhoundRuntime.Live.unsafeRun {
+    ZIO
+      .runtime[Blocking with GreyhoundMetrics]
+      .map(runtime =>
+        new AdminClient {
+          override def createTopics(configs: Set[TopicConfig]): Future[Map[String, Option[Throwable]]] =
+            runtime.unsafeRunToFuture {
+              AdminClientCore.make(config).use { client => client.createTopics(configs) }
+            }
+
+          override def createTopic(config: TopicConfig): Future[Unit] =
+            createTopics(Set(config)).flatMap { errors =>
+              errors
+                .get(config.name)
+                .map(maybeError => maybeError.map(error => Future.failed(error)).getOrElse(Future.unit))
+                .getOrElse(Future.unit)
+            }
+
+          override def numberOfBrokers: Future[Int] =
+            runtime.unsafeRunToFuture(AdminClientCore.make(config).use(_.numberOfBrokers))
+
+          override def propertiesFor(topic: Topic): Future[TopicPropertiesResult] =
+            runtime.unsafeRunToFuture(AdminClientCore.make(config).use { client =>
+              client.propertiesFor(Set(topic)).map(_.getOrElse(topic, TopicPropertiesResult.TopicDoesnExist(topic)))
+            })
         }
-
-      override def createTopic(config: TopicConfig): Future[Unit] =
-        createTopics(Set(config)).flatMap { errors =>
-          errors
-            .get(config.name)
-            .map(maybeError => maybeError.map(error => Future.failed(error)).getOrElse(Future.unit))
-            .getOrElse(Future.unit)
-        }
-
-      override def numberOfBrokers: Future[Int] =
-        GreyhoundRuntime.Live.unsafeRunToFuture(ZIO.scoped(AdminClientCore.make(config).flatMap(_.numberOfBrokers)))
-
-      override def propertiesFor(topic: Topic): Future[TopicPropertiesResult] =
-        GreyhoundRuntime.Live.unsafeRunToFuture(ZIO.scoped(AdminClientCore.make(config).flatMap { client =>
-          client.propertiesFor(Set(topic)).map(_.getOrElse(topic, TopicPropertiesResult.TopicDoesnExist(topic)))
-        }))
-    }
+      )
+  }
 }
