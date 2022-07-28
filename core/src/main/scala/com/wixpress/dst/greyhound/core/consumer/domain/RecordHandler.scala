8ed68026aa4328e62d7986a1098bcac004040256
Noam Berman <noambe@wix.com>
2022-07-28 17:40:58 +0300

    Revert "Zio2 preview - will be reverted immediately (#28367)" (#28373)
    This reverts commit 12187c1be5e7f891e988e71f2ca36ceaf62e3cc0.
    
    GitOrigin-RevId: 327da6f99384e30ca21b76aa8e9ae2aa9b5cdb80

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/domain/RecordHandler.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/domain/RecordHandler.scala
@@ -13,12 +13,12 @@ trait RecordHandler[-R, +E, K, V] {
   /**
    * Handle a single record.
    */
-  def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[R, E, Any]
+  def handle(record: ConsumerRecord[K, V]): ZIO[R, E, Any]
 
   /**
    * Returns a handler which adapts the input by using an pure function `f`.
    */
-  def contramap[K2, V2](f: ConsumerRecord[K2, V2] => ConsumerRecord[K, V])(implicit trace: Trace): RecordHandler[R, E, K2, V2] =
+  def contramap[K2, V2](f: ConsumerRecord[K2, V2] => ConsumerRecord[K, V]): RecordHandler[R, E, K2, V2] =
     contramapM(record => ZIO.succeed(f(record)))
 
   /**
@@ -39,7 +39,7 @@ trait RecordHandler[-R, +E, K, V] {
    */
   def contramapM[R1 <: R, E1 >: E, K2, V2](f: ConsumerRecord[K2, V2] => ZIO[R1, E1, ConsumerRecord[K, V]]): RecordHandler[R1, E1, K2, V2] =
     new RecordHandler[R1, E1, K2, V2] {
-      override def handle(record: ConsumerRecord[K2, V2])(implicit trace: Trace): ZIO[R1, E1, Any] =
+      override def handle(record: ConsumerRecord[K2, V2]): ZIO[R1, E1, Any] =
         f(record).flatMap(self.handle)
     }
 
@@ -48,7 +48,7 @@ trait RecordHandler[-R, +E, K, V] {
    */
   def mapError[E2](f: E => E2): RecordHandler[R, E2, K, V] =
     new RecordHandler[R, E2, K, V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[R, E2, Any] =
+      override def handle(record: ConsumerRecord[K, V]): ZIO[R, E2, Any] =
         self.handle(record).mapError(f)
     }
 
@@ -60,15 +60,12 @@ trait RecordHandler[-R, +E, K, V] {
    *
    *   val recoveringHandler: RecordHandler[Console, Nothing, Int, String] =
    *     failingHandler.withErrorHandler { error =>
-   *       zio.ZIO.debug(error.getMessage)
+   *       zio.console.putStrLn(error.getMessage)
    *     }
    * }}}
    */
   def withErrorHandler[R1 <: R, E2](f: (E, ConsumerRecord[K, V]) => ZIO[R1, E2, Any]): RecordHandler[R1, E2, K, V] =
-    new RecordHandler[R1,E2,K,V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[R1, E2, Any] =
-        self.handle(record).catchAll(e => f(e, record))
-    }
+    (record: ConsumerRecord[K, V]) => self.handle(record).catchAll(e => f(e, record))
 
   /**
    * Return a handler which can handle errors as [[Cause[E]]] (which includes [[Cause.Die]] and fiber traces), potentially eliminating them.
@@ -78,15 +75,12 @@ trait RecordHandler[-R, +E, K, V] {
    *
    *   val recoveringHandler: RecordHandler[Console, Nothing, Int, String] =
    *     failingHandler.withCauseHandler { cause =>
-   *       zio.ZIO.debug(cause.squashTrace)
+   *       zio.console.putStrLn(cause.squashTrace)
    *     }
    * }}}
    */
   def withErrorCauseHandler[R1 <: R, E2](f: (Cause[E], ConsumerRecord[K, V]) => ZIO[R1, E2, Any]): RecordHandler[R1, E2, K, V] =
-    new RecordHandler[R1,E2,K,V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[R1, E2, Any] =
-        self.handle(record).catchAllCause(e => f(e, record))
-    }
+    (record: ConsumerRecord[K, V]) => self.handle(record).catchAllCause(e => f(e, record))
 
   /**
    * Recover from all errors by ignoring them.
@@ -97,10 +91,10 @@ trait RecordHandler[-R, +E, K, V] {
   /**
    * Provides the handler with its required environment, which eliminates its dependency on `R`.
    */
-  def provide[R1 <: R: Tag](r: R1): RecordHandler[Any, E, K, V] =
+  def provide(r: R): RecordHandler[Any, E, K, V] =
     new RecordHandler[Any, E, K, V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[Any, E, Any] =
-        self.handle(record).provide(ZLayer.succeed(r))
+      override def handle(record: ConsumerRecord[K, V]): ZIO[Any, E, Any] =
+        self.handle(record).provide(r)
     }
 
   /**
@@ -108,7 +102,7 @@ trait RecordHandler[-R, +E, K, V] {
    */
   def andThen[R1 <: R, E1 >: E](f: ConsumerRecord[K, V] => ZIO[R1, E1, Any]): RecordHandler[R1, E1, K, V] =
     new RecordHandler[R1, E1, K, V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[R1, E1, Any] =
+      override def handle(record: ConsumerRecord[K, V]): ZIO[R1, E1, Any] =
         self.handle(record) *> f(record)
     }
 
@@ -118,19 +112,19 @@ trait RecordHandler[-R, +E, K, V] {
   def withDeserializers(
     keyDeserializer: Deserializer[K],
     valueDeserializer: Deserializer[V]
-  )(implicit trace: Trace): RecordHandler[R, Either[SerializationError, E], Chunk[Byte], Chunk[Byte]] =
+  ): RecordHandler[R, Either[SerializationError, E], Chunk[Byte], Chunk[Byte]] =
     mapError(Right(_)).contramapM { record =>
       record
         .bimapM(
           key => keyDeserializer.deserialize(record.topic, record.headers, key),
-          value => Option(value).fold(ZIO.attempt[V](null.asInstanceOf[V]))(v => valueDeserializer.deserialize(record.topic, record.headers, v))
+          value => Option(value).fold(Task[V](null.asInstanceOf[V]))(v => valueDeserializer.deserialize(record.topic, record.headers, v))
         )
         .mapError(e => Left(SerializationError(e)))
     }
 
   def withDecryptor[E1 >: E, R1 <: R](dec: Decryptor[R1, E1, K, V]) = {
     new RecordHandler[R1, E1, K, V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace) =
+      override def handle(record: ConsumerRecord[K, V]) =
         dec.decrypt(record).flatMap(self.handle)
     }
   }
@@ -138,24 +132,22 @@ trait RecordHandler[-R, +E, K, V] {
   /**
    * creates a RecordHandler that will run the given function before running this handlers' `handle`
    */
-  def <*[R1 <: R](f: ConsumerRecord[K, V] => URIO[R1, Unit]): RecordHandler[R1, E, K, V] =
-    new RecordHandler[R1, E,K,V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): ZIO[R1, E, Any] =
-        f(record) *> self.handle(record)
-    }
+  def <*[R1 <: R](f: ConsumerRecord[K, V] => URIO[R1, Unit]): RecordHandler[R1, E, K, V] = (record: ConsumerRecord[K, V]) => {
+    f(record) *> self.handle(record)
   }
+}
 
 case class SerializationError(cause: Throwable) extends RuntimeException(cause)
 
 object RecordHandler {
   def apply[R, E, K, V](f: ConsumerRecord[K, V] => ZIO[R, E, Any]): RecordHandler[R, E, K, V] = {
     new RecordHandler[R, E, K, V] {
-      override def handle(record: ConsumerRecord[K, V]) (implicit trace: Trace): ZIO[R, E, Any] = f(record)
+      override def handle(record: ConsumerRecord[K, V]): ZIO[R, E, Any] = f(record)
     }
   }
 
   def empty[K, V]: RecordHandler[Any, Nothing, K, V] =
     new RecordHandler[Any, Nothing, K, V] {
-      override def handle(record: ConsumerRecord[K, V])(implicit trace: Trace): UIO[Any] = ZIO.unit
+      override def handle(record: ConsumerRecord[K, V]): UIO[Any] = ZIO.unit
     }
 }
