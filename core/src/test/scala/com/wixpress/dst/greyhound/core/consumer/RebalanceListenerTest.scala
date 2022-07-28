05b64f7d6fe7c915dec89cc96b93fa3f6db77726
Noam Berman <noambe@wix.com>
2022-07-28 11:20:54 +0300

    Revert "Zio2 preview (#28356)" (#28363)
    This reverts commit 9701068a72b8d713289659661be9c1ecfd4cc0ae.
    
    GitOrigin-RevId: 72978a69b86816d12213455b44949dec8a3e407c

--- core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/RebalanceListenerTest.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/RebalanceListenerTest.scala
@@ -1,28 +1,27 @@
 package com.wixpress.dst.greyhound.core.consumer
 
 import com.wixpress.dst.greyhound.core.TopicPartition
-import zio.{Ref, URIO, ZIO, Trace}
+import zio.{Ref, URIO, ZIO}
 import zio.test.junit.JUnitRunnableSpec
 import zio.test._
 import zio.test.Assertion._
 
 class RebalanceListenerTest extends JUnitRunnableSpec {
-  private implicit val trace = Trace.empty
   def spec = suite("RebalanceListenerTest")(
-    test("*> operator") {
+    testM("*> operator") {
       for {
         loggedRef <- Ref.make(Vector.empty[String])
         log        = (s: String) => loggedRef.update(_ :+ s)
         runtime   <- ZIO.runtime[Any]
-        unsafeLog  = (s: String) => zio.Unsafe.unsafe { implicit ss => runtime.unsafe.run(log(s)).getOrThrowFiberFailure() }
+        unsafeLog  = (s: String) => runtime.unsafeRunTask(log(s))
         listener   = (id: String) =>
                        new RebalanceListener[Any] {
                          override def onPartitionsRevoked(
                            consumer: Consumer,
                            partitions: Set[TopicPartition]
-                         ) (implicit trace: Trace): URIO[Any, DelayedRebalanceEffect] =
+                         ): URIO[Any, DelayedRebalanceEffect] =
                            log(s"$id.revoke $partitions").as(DelayedRebalanceEffect(unsafeLog(s"$id.revoke.tle $partitions")))
-                         override def onPartitionsAssigned(consumer: Consumer, partitions: Set[TopicPartition]) (implicit trace: Trace): URIO[Any, Any] =
+                         override def onPartitionsAssigned(consumer: Consumer, partitions: Set[TopicPartition]): URIO[Any, Any] =
                            log(s"$id.assigned $partitions")
                        }
         l1l2       = listener("l1") *> listener("l2")
