ea69d667ce444e9b9c3799a12be470c4a0c07939
Noam Berman <noambe@wix.com>
2022-08-18 00:38:11 +0300

    zio 2 upgrade (#28392)
    * zio 2 upgrade #pr #skipreview #draft #draftpr
    
    * .
    
    * fix core-server-build-tools branch
    
    * update branches
    
    * .
    
    * .
    
    * cross repo checks
    
    * cross repo
    
    * update branches
    
    * .
    
    * remove sttp from greyhound
    
    * remove tag from R in zcallscope
    
    * [WIO] ZIO 2.0 migration - fixed cats laws tests (#28631)
    
    * core-server-build-tools
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * scalacheck upgrade
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * returned withoutLogStash for migration purposes
    
    * .
    
    * .
    
    * .
    
    * testing without health test
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Update cross_repo_checks_zio2-cont2.json
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * add newrelic support; remove prints;
    
    * .
    
    Co-authored-by: Alexander Sidorenko <aleks-sidorenko@users.noreply.github.com>
    Co-authored-by: Leon Burdinov <leonidb@wix.com>
    GitOrigin-RevId: 5cda6910be5b3c71584e7f3b4a37d3da090dbafd

--- core/src/it/scala/com/wixpress/dst/greyhound/testenv/ITEnv.scala
+++ core/src/it/scala/com/wixpress/dst/greyhound/testenv/ITEnv.scala
@@ -2,26 +2,35 @@ package com.wixpress.dst.greyhound.testenv
 
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.core.producer.{Producer, ProducerConfig, ReportingProducer}
-import com.wixpress.dst.greyhound.core.testkit.TestMetrics
+import com.wixpress.dst.greyhound.core.testkit.{TestMetrics, TestMetricsEnvironment}
 import com.wixpress.dst.greyhound.core.{CleanupPolicy, TopicConfig}
 import com.wixpress.dst.greyhound.testkit.{ManagedKafka, ManagedKafkaConfig}
-import zio.duration._
-import zio.{random, test, UManaged, ZIO, ZManaged}
+import zio.Random.nextIntBetween
+import zio.{Random, Scope, Trace, ZEnvironment, ZIO, ZLayer, durationInt}
+import zio.managed._
+import zio.test.{Live, TestClock}
 
 object ITEnv {
-  type Env = TestMetrics with zio.ZEnv
+    implicit val trace = Trace.empty
+  type Env = TestMetrics
   case class TestResources(kafka: ManagedKafka, producer: ReportingProducer[Any])
 
-  def ManagedEnv: UManaged[zio.ZEnv with TestMetrics] =
+  def ManagedEnv: UManaged[ ZEnvironment[TestMetrics with GreyhoundMetrics]] =
     for {
-      env         <- (GreyhoundMetrics.liveLayer ++ test.environment.liveEnvironment).build
-      testMetrics <- TestMetrics.make
-    } yield env ++ testMetrics
+      env         <- ZManaged.fromZIO(GreyhoundMetrics.liveLayer.build.provide(ZLayer.succeed(Scope.global)))
+      testMetrics <- ZManaged.succeed(TestMetricsEnvironment)
+      a = env ++ testMetrics
+    } yield a
 
-  def testResources(): ZManaged[zio.ZEnv with TestMetrics, Throwable, TestResources] = {
+  def testResources(): ZManaged[TestMetrics with GreyhoundMetrics, Throwable, TestResources] = {
+    val nextPort = nextIntBetween(10100, 60001)
+
     for {
-      kafka    <- ManagedKafka.make(ManagedKafkaConfig.Default)
-      producer <- Producer.makeR[Any](ProducerConfig(kafka.bootstrapServers))
+      kafkaPort <- nextPort.toManaged
+      zkPort <- nextPort .toManaged
+      gM <- ZManaged.fromZIO(ZIO.environment[GreyhoundMetrics])
+      kafka    <- ZManaged.acquireReleaseWith(ManagedKafka.make(ManagedKafkaConfig(kafkaPort, zkPort, Map.empty)).provideEnvironment(gM ++ ZEnvironment(Scope.global)))(_.shutdown.ignore)
+      producer <- ZManaged.acquireReleaseWith(Producer.makeR[Any](ProducerConfig(kafka.bootstrapServers)).provide(ZLayer.succeed(Scope.global)))(_.shutdown)
     } yield TestResources(kafka, ReportingProducer(producer))
   }
 
@@ -34,7 +43,7 @@ object ITEnv {
   def randomAlphaLowerChar = {
     val low  = 'a'.toInt
     val high = 'z'.toInt + 1
-    random.nextIntBetween(low, high).map(_.toChar)
+    nextIntBetween(low, high).map(_.toChar)
   }
 
   val randomId    = ZIO.collectAll(List.fill(6)(randomAlphaLowerChar)).map(_.mkString)
@@ -47,3 +56,57 @@ object ITEnv {
     } yield topic
   }
 }
+
+
+
+//package com.wixpress.dst.greyhound.testenv
+//
+//import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
+//import com.wixpress.dst.greyhound.core.producer.{Producer, ProducerConfig, ReportingProducer}
+//import com.wixpress.dst.greyhound.core.testkit.TestMetrics
+//import com.wixpress.dst.greyhound.core.{CleanupPolicy, TopicConfig}
+//import com.wixpress.dst.greyhound.testkit.{ManagedKafka, ManagedKafkaConfig}
+//import zio.{Trace, ZIO, ZLayer, ZManaged, durationInt, random, test}
+//import zio.managed._
+//
+//object ITEnv {
+//  implicit val trace = Trace.empty
+//  type Env = TestMetrics
+//  case class TestResources(kafka: ManagedKafka, producer: ReportingProducer[Any])
+//
+//  def ManagedEnv: UManaged[TestMetrics] =
+//    (ZLayer.succeed(TestMetrics.make) ++ ZLayer.succeed(GreyhoundMetrics.live)).build.map(_.)
+////    for {
+////      testMetrics <-
+////      env         = (GreyhoundMetrics.live)
+////    } yield env ++ testMetrics
+//
+//  def testResources(): ZManaged[TestMetrics, Throwable, TestResources] = {
+//    for {
+//      kafka    <- ManagedKafka.make(ManagedKafkaConfig.Default)
+//      producer <- Producer.makeR[Any](ProducerConfig(kafka.bootstrapServers))
+//    } yield TestResources(kafka, ReportingProducer(producer))
+//  }
+//
+//  def clientId              = randomId.map(id => s"greyhound-consumers-$id")
+//  def clientId(tag: String) = randomId.map(id => s"client-$tag-$id")
+//
+//  val partitions = 4
+//  val delete     = CleanupPolicy.Delete(1.hour.toMillis)
+//
+//  def randomAlphaLowerChar = {
+//    val low  = 'a'.toInt
+//    val high = 'z'.toInt + 1
+//    random.nextIntBetween(low, high).map(_.toChar)
+//  }
+//
+//  val randomId    = ZIO.collectAll(List.fill(6)(randomAlphaLowerChar)).map(_.mkString)
+//  val randomGroup = randomId.map(id => s"group-$id")
+//
+//  implicit class ManagedKafkaOps(val kafka: ManagedKafka) {
+//    def createRandomTopic(partitions: Int = partitions, prefix: String = "topic", params: Map[String, String] = Map.empty) = for {
+//      topic <- randomId.map(id => s"$prefix-$id")
+//      _     <- kafka.createTopic(TopicConfig(topic, partitions, 1, delete, params))
+//    } yield topic
+//  }
+//}
