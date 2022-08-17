ef920e961e438044108584506b191809c2a07570
Noam Berman <noambe@wix.com>
2022-08-18 01:15:42 +0300

    Revert "zio 2 upgrade" (#28773)
    Revert "zio 2 upgrade (#28392)"
    
    This reverts commit 5cda6910be5b3c71584e7f3b4a37d3da090dbafd.
    
    GitOrigin-RevId: 5a85766ee2112462dd90e1b8d28a55dcea650afa

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/package.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/package.scala
@@ -5,7 +5,8 @@ import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerSubscription
 import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerSubscription.{TopicPattern, Topics}
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
-import zio.{RIO, Tag, Trace, ZIO}
+import zio.{RIO, ZIO}
+import zio.blocking.Blocking
 import org.apache.kafka.common.{TopicPartition => KafkaTopicPartition}
 
 import scala.collection.JavaConverters._
@@ -13,13 +14,13 @@ import scala.collection.JavaConverters._
 package object consumer {
   def subscribe[R](subscription: ConsumerSubscription, listener: RebalanceListener[R])(
     consumer: Consumer
-  )(implicit trace: Trace): RIO[GreyhoundMetrics with R, Unit] =
+  ): RIO[Blocking with GreyhoundMetrics with R, Unit] =
     ZIO.whenCase(subscription) {
       case TopicPattern(pattern, _) =>
         consumer.subscribePattern(pattern, listener)
       case Topics(topics)           =>
         consumer.subscribe(topics, listener)
-    }.unit
+    }
 
   def kafkaOffsets(offsets: Map[TopicPartition, Offset]): util.Map[KafkaTopicPartition, OffsetAndMetadata] =
     offsets.map { case (tp, offset) => tp.asKafka -> new OffsetAndMetadata(offset) }.asJava
