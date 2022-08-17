bcd6e44f268b3e8dd86fc283db6f9e7824fe7323
Noam Berman <noambe@wix.com>
2022-08-18 01:15:42 +0300

    Revert "zio 2 upgrade" (#28773)
    Revert "zio 2 upgrade (#28392)"
    
    This reverts commit 5cda6910be5b3c71584e7f3b4a37d3da090dbafd.
    
    GitOrigin-RevId: 5a85766ee2112462dd90e1b8d28a55dcea650afa

--- core/src/main/scala/com/wixpress/dst/greyhound/core/producer/Encryptor.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/producer/Encryptor.scala
@@ -1,11 +1,11 @@
 package com.wixpress.dst.greyhound.core.producer
 
-import zio.{Chunk, Task, Trace, ZIO}
+import zio.{Chunk, Task, UIO}
 
 trait Encryptor {
-  def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]) (implicit trace: Trace): Task[ProducerRecord[K, Chunk[Byte]]]
+  def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]): Task[ProducerRecord[K, Chunk[Byte]]]
 }
 
 case object NoOpEncryptor extends Encryptor {
-  override def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]) (implicit trace: Trace): Task[ProducerRecord[K, Chunk[Byte]]] = ZIO.succeed(record)
+  override def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]): Task[ProducerRecord[K, Chunk[Byte]]] = UIO(record)
 }
