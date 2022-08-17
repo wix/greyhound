975e2b9c2423929b48465c73648c8f08158f3cb4
Noam Berman <noambe@wix.com>
2022-08-18 01:15:42 +0300

    Revert "zio 2 upgrade" (#28773)
    Revert "zio 2 upgrade (#28392)"
    
    This reverts commit 5cda6910be5b3c71584e7f3b4a37d3da090dbafd.
    
    GitOrigin-RevId: 5a85766ee2112462dd90e1b8d28a55dcea650afa

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
@@ -3,23 +3,20 @@ package com.wixpress.dst.greyhound.core
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.core.producer.ProducerRecord
 import zio.Schedule._
-
-import zio._
+import zio.{Chunk, Has, RIO, Ref, Schedule, UIO, ZIO}
+import zio.clock.Clock
+import zio.duration._
 
 package object testkit {
-  implicit val trace = Trace.empty
-  type TestMetrics = TestMetrics.Service with GreyhoundMetrics.Service
-  val TestMetricsEnvironment = ZEnvironment(GreyhoundMetrics.live) ++
-    zio.Unsafe.unsafe { implicit s => zio.Runtime.default.unsafe.run(TestMetrics.makeEnv()).getOrThrowFiberFailure() }
+  type TestMetrics = Has[TestMetrics.Service] with GreyhoundMetrics
 
-  def eventuallyZ[R <: Any, T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
+  def eventuallyZ[R <: Has[_], T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
     eventuallyTimeoutFail(f)(predicate)(timeout)
 
-  def eventuallyNotZ[R <: Any, T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
+  def eventuallyNotZ[R <: Has[_], T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
     eventuallyNotTimeoutFail(f)(predicate)(timeout)
 
-  def eventuallyTimeoutFail[R <: Any, T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] = {
-    implicit val trace = Trace.empty
+  def eventuallyTimeoutFail[R <: Has[_], T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] =
     for {
       timeoutRes <- eventuallyTimeout(f)(predicate)(timeout)
       result      = timeoutRes.map(_._2)
@@ -27,10 +24,8 @@ package object testkit {
                       ZIO.fail(new RuntimeException(s"eventuallyZ predicate failed after ${timeout.toMillis} milliseconds. result: $result"))
                     )
     } yield ()
-  }
 
-  def eventuallyNotTimeoutFail[R <: Any, T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] = {
-    implicit val trace = Trace.empty
+  def eventuallyNotTimeoutFail[R <: Has[_], T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] =
     for {
       timeoutRes <- eventuallyTimeout(f)(predicate)(timeout)
       result      = timeoutRes.map(_._2)
@@ -38,18 +33,16 @@ package object testkit {
                       ZIO.fail(new RuntimeException(s"eventuallyZ predicate failed after ${timeout.toMillis} milliseconds. result: $result"))
                     )
     } yield ()
-  }
 
-  def eventuallyTimeout[R <: Any, T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Option[(Long, T)]] = {
-    implicit val trace = Trace.empty
+  def eventuallyTimeout[R <: Has[_], T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Option[(Long, T)]] =
     for {
       resultRef  <- Ref.make[Option[T]](None)
       timeoutRes <- f
-                      .flatMap(r => resultRef.set(Some(r)) *> ZIO.succeed(r))
+                      .flatMap(r => resultRef.set(Some(r)) *> UIO(r))
                       .repeat(spaced(100.millis) && Schedule.recurUntil(predicate))
                       .timeout(timeout)
+                      .provideSomeLayer[R](Clock.live)
     } yield timeoutRes
-  }
 
   implicit class StringOps(val str: String)                                              {
     def bytesChunk = Chunk.fromArray(str.getBytes("UTF8"))
