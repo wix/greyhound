ff93c48377a2235997ea4dbb113d7cf814c1e4bf
Noam Berman <noambe@wix.com>
2022-08-18 00:38:11 +0300

    zio 2 upgrade (#28392)
    * zio 2 upgrade #pr #skipreview #draft #draftpr
    
    * .
    
    * fix core-server-build-tools branch
    
    * update branches
    
    * .
    
    * .
    
    * cross repo checks
    
    * cross repo
    
    * update branches
    
    * .
    
    * remove sttp from greyhound
    
    * remove tag from R in zcallscope
    
    * [WIO] ZIO 2.0 migration - fixed cats laws tests (#28631)
    
    * core-server-build-tools
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * scalacheck upgrade
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * returned withoutLogStash for migration purposes
    
    * .
    
    * .
    
    * .
    
    * testing without health test
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Update cross_repo_checks_zio2-cont2.json
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * add newrelic support; remove prints;
    
    * .
    
    Co-authored-by: Alexander Sidorenko <aleks-sidorenko@users.noreply.github.com>
    Co-authored-by: Leon Burdinov <leonidb@wix.com>
    GitOrigin-RevId: 5cda6910be5b3c71584e7f3b4a37d3da090dbafd

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/package.scala
@@ -3,20 +3,23 @@ package com.wixpress.dst.greyhound.core
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.core.producer.ProducerRecord
 import zio.Schedule._
-import zio.{Chunk, Has, RIO, Ref, Schedule, UIO, ZIO}
-import zio.clock.Clock
-import zio.duration._
+
+import zio._
 
 package object testkit {
-  type TestMetrics = Has[TestMetrics.Service] with GreyhoundMetrics
+  implicit val trace = Trace.empty
+  type TestMetrics = TestMetrics.Service with GreyhoundMetrics.Service
+  val TestMetricsEnvironment = ZEnvironment(GreyhoundMetrics.live) ++
+    zio.Unsafe.unsafe { implicit s => zio.Runtime.default.unsafe.run(TestMetrics.makeEnv()).getOrThrowFiberFailure() }
 
-  def eventuallyZ[R <: Has[_], T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
+  def eventuallyZ[R <: Any, T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
     eventuallyTimeoutFail(f)(predicate)(timeout)
 
-  def eventuallyNotZ[R <: Has[_], T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
+  def eventuallyNotZ[R <: Any, T](f: RIO[R, T], timeout: Duration = 4.seconds)(predicate: T => Boolean): ZIO[R, Throwable, Unit] =
     eventuallyNotTimeoutFail(f)(predicate)(timeout)
 
-  def eventuallyTimeoutFail[R <: Has[_], T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] =
+  def eventuallyTimeoutFail[R <: Any, T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] = {
+    implicit val trace = Trace.empty
     for {
       timeoutRes <- eventuallyTimeout(f)(predicate)(timeout)
       result      = timeoutRes.map(_._2)
@@ -24,8 +27,10 @@ package object testkit {
                       ZIO.fail(new RuntimeException(s"eventuallyZ predicate failed after ${timeout.toMillis} milliseconds. result: $result"))
                     )
     } yield ()
+  }
 
-  def eventuallyNotTimeoutFail[R <: Has[_], T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] =
+  def eventuallyNotTimeoutFail[R <: Any, T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Unit] = {
+    implicit val trace = Trace.empty
     for {
       timeoutRes <- eventuallyTimeout(f)(predicate)(timeout)
       result      = timeoutRes.map(_._2)
@@ -33,16 +38,18 @@ package object testkit {
                       ZIO.fail(new RuntimeException(s"eventuallyZ predicate failed after ${timeout.toMillis} milliseconds. result: $result"))
                     )
     } yield ()
+  }
 
-  def eventuallyTimeout[R <: Has[_], T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Option[(Long, T)]] =
+  def eventuallyTimeout[R <: Any, T](f: RIO[R, T])(predicate: T => Boolean)(timeout: Duration): ZIO[R, Throwable, Option[(Long, T)]] = {
+    implicit val trace = Trace.empty
     for {
       resultRef  <- Ref.make[Option[T]](None)
       timeoutRes <- f
-                      .flatMap(r => resultRef.set(Some(r)) *> UIO(r))
+                      .flatMap(r => resultRef.set(Some(r)) *> ZIO.succeed(r))
                       .repeat(spaced(100.millis) && Schedule.recurUntil(predicate))
                       .timeout(timeout)
-                      .provideSomeLayer[R](Clock.live)
     } yield timeoutRes
+  }
 
   implicit class StringOps(val str: String)                                              {
     def bytesChunk = Chunk.fromArray(str.getBytes("UTF8"))
