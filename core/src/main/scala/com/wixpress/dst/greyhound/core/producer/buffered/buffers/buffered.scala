78c6d1380ecce101331f045a799ce18cd51f0591
Noam Berman <noambe@wix.com>
2022-08-20 22:59:41 +0300

    ZIO 2 upgrade (#28802)
    * Revert "Revert "zio 2 upgrade" (#28773)"
    
    This reverts commit 5a85766ee2112462dd90e1b8d28a55dcea650afa.
    
    * admin client: fix closing
    
    * Update and rename zio2-cont2_2nd_party_resolved_dependencies.bzl to zio2-6_2nd_party_resolved_dependencies.bzl
    
    * fix branches
    
    * .
    
    * .
    
    GitOrigin-RevId: 38c516f8086fdd91ff200768e22ef6f6acd1ce0d

--- core/src/main/scala/com/wixpress/dst/greyhound/core/producer/buffered/buffers/buffered.scala
+++
@@ -1,9 +0,0 @@
-package com.wixpress.dst.greyhound.core.producer.buffered.buffers
-
-package object buffers {
-  type PersistedMessageId = Long
-
-  object PersistedMessageId {
-    val notPersisted = -1
-  }
-}
