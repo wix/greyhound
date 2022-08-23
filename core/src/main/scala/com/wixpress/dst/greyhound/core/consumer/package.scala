224f66abc30d0da8a80d55c78429646c094c9f2f
Noam Berman <noambe@wix.com>
2022-08-23 16:07:56 +0300

    zio 2 (#28812)
    * merge master #draft #draftpr #skipreview #pr
    
    * .
    
    * Update and rename zio2-6_2nd_party_resolved_dependencies.bzl to zio2-7_2nd_party_resolved_dependencies.bzl
    
    * .
    
    * .
    
    * .
    
    * add dynamo streams health test
    
    * .
    
    * fix ht loom
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    GitOrigin-RevId: 9e08ec6f46b94eeb5016fd9bdc58fa101be385df

--- core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/package.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/consumer/package.scala
@@ -5,8 +5,7 @@ import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerSubscription
 import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerSubscription.{TopicPattern, Topics}
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
-import zio.{RIO, ZIO}
-import zio.blocking.Blocking
+import zio.{RIO, Tag, Trace, ZIO}
 import org.apache.kafka.common.{TopicPartition => KafkaTopicPartition}
 
 import scala.collection.JavaConverters._
@@ -14,13 +13,13 @@ import scala.collection.JavaConverters._
 package object consumer {
   def subscribe[R](subscription: ConsumerSubscription, listener: RebalanceListener[R])(
     consumer: Consumer
-  ): RIO[Blocking with GreyhoundMetrics with R, Unit] =
+  )(implicit trace: Trace): RIO[GreyhoundMetrics with R, Unit] =
     ZIO.whenCase(subscription) {
       case TopicPattern(pattern, _) =>
         consumer.subscribePattern(pattern, listener)
       case Topics(topics)           =>
         consumer.subscribe(topics, listener)
-    }
+    }.unit
 
   def kafkaOffsets(offsets: Map[TopicPartition, Offset]): util.Map[KafkaTopicPartition, OffsetAndMetadata] =
     offsets.map { case (tp, offset) => tp.asKafka -> new OffsetAndMetadata(offset) }.asJava
