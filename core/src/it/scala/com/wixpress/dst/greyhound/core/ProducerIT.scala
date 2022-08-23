72d65625f1c033f44c96a356b2267946d050810b
Noam Berman <noambe@wix.com>
2022-08-23 19:36:58 +0300

    Revert "ZIO 2" (#28897)
    Revert "ZIO 2 (#28895)"
    
    This reverts commit b1c7fb94582b4323b1570cd458e943dc914b34fd.
    
    GitOrigin-RevId: e0a85f8cf80a3e5fb96b858497fe3797222046be

--- core/src/it/scala/com/wixpress/dst/greyhound/core/ProducerIT.scala
+++ core/src/it/scala/com/wixpress/dst/greyhound/core/ProducerIT.scala
@@ -6,11 +6,12 @@ import com.wixpress.dst.greyhound.core.testkit.BaseTestWithSharedEnv
 import com.wixpress.dst.greyhound.testenv.ITEnv
 import com.wixpress.dst.greyhound.testenv.ITEnv._
 import zio._
+import zio.duration._
 
 class ProducerIT extends BaseTestWithSharedEnv[Env, TestResources] {
   sequential
 
-  override def env = ITEnv.ManagedEnv
+  override def env: UManaged[ITEnv.Env] = ITEnv.ManagedEnv
 
   override def sharedEnv = ITEnv.testResources()
 
@@ -18,35 +19,32 @@ class ProducerIT extends BaseTestWithSharedEnv[Env, TestResources] {
 
   "produce async" in {
     for {
-      r                             <- getShared
-      TestResources(kafka, producer) = r
-      topic                         <- kafka.createRandomTopic(2)
-      kafkaIO                       <- producer.produceAsync(record(topic), StringSerde, IntSerde)
-      result                        <- kafkaIO
+      TestResources(kafka, producer) <- getShared
+      topic                          <- kafka.createRandomTopic(2)
+      kafkaIO                        <- producer.produceAsync(record(topic), StringSerde, IntSerde)
+      result                         <- kafkaIO
     } yield result === RecordMetadata(topic, partition = 1, offset = 0L)
   }
 
   "produce" in {
     for {
-      r                             <- getShared
-      TestResources(kafka, producer) = r
-      topic                         <- kafka.createRandomTopic(2)
-      result                        <- producer.produce(record(topic), StringSerde, IntSerde)
+      TestResources(kafka, producer) <- getShared
+      topic                          <- kafka.createRandomTopic(2)
+      result                         <- producer.produce(record(topic), StringSerde, IntSerde)
     } yield result === RecordMetadata(topic, partition = 1, offset = 0L)
   }
 
   "produce null value (tombstone)" in {
     for {
-      r                             <- getShared
-      TestResources(kafka, producer) = r
-      topic                         <- kafka.createRandomTopic(2)
-      result                        <- producer.produce(ProducerRecord[String, String](topic, null, partition = Some(1)), StringSerde, StringSerde)
+      TestResources(kafka, producer) <- getShared
+      topic                          <- kafka.createRandomTopic(2)
+      result                         <- producer.produce(ProducerRecord[String, String](topic, null, partition = Some(1)), StringSerde, StringSerde)
     } yield result === RecordMetadata(topic, partition = 1, offset = 0L)
   }
 
-  "map to failure" in ZIO.scoped {
+  "map to failure" in {
     for {
-      _ <- Producer.makeR[Any](failFastInvalidBrokersConfig).flatMap { producer =>
+      _ <- Producer.makeR[Any](failFastInvalidBrokersConfig).use { producer =>
              for {
                failure <- producer.produce(record("no_such_topic"), StringSerde, IntSerde).flip
              } yield failure.getClass.getSimpleName === "TimeoutError"
