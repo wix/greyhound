aa420a0db2d90ba2045cbe2b1aa2cae6e4779864
Noam Berman <noambe@wix.com>
2022-08-23 16:48:13 +0300

    Revert zio2 (#28890)
    * Revert "fix commit that slipped between the last merge (#28888)"
    
    This reverts commit 96b1bdc5639e0ea43d9b41b5ab208fb6788b33b9.
    
    * Revert "zio 2 (#28812)"
    
    This reverts commit 9e08ec6f46b94eeb5016fd9bdc58fa101be385df.
    
    GitOrigin-RevId: e3575f9745e4228da247c44d49b54873b2a90f84

--- core/src/main/scala/com/wixpress/dst/greyhound/core/producer/ReportingProducer.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/producer/ReportingProducer.scala
@@ -2,59 +2,60 @@ package com.wixpress.dst.greyhound.core.producer
 
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeUnit.MILLISECONDS
+import _root_.zio.blocking.Blocking
 import com.wixpress.dst.greyhound.core.PartitionInfo
 import com.wixpress.dst.greyhound.core.metrics.{GreyhoundMetric, GreyhoundMetrics}
 import com.wixpress.dst.greyhound.core.producer.ProducerMetric._
-import zio.{Chunk, IO, RIO, Trace, ULayer, ZIO}
+import zio.clock.{currentTime, Clock}
+import zio.{Chunk, IO, RIO, ULayer, ZIO}
 import GreyhoundMetrics._
 
 import scala.concurrent.duration.FiniteDuration
-import zio.Clock.currentTime
 
 case class ReportingProducer[-R](internal: ProducerR[R], extraAttributes: Map[String, String])
-    extends ProducerR[GreyhoundMetrics with R] {
+    extends ProducerR[GreyhoundMetrics with Clock with R] {
 
   override def produceAsync(
     record: ProducerRecord[Chunk[Byte], Chunk[Byte]]
-  )(implicit trace: Trace): ZIO[GreyhoundMetrics with R, ProducerError, IO[ProducerError, RecordMetadata]] =
+  ): ZIO[Blocking with Clock with GreyhoundMetrics with R, ProducerError, IO[ProducerError, RecordMetadata]] =
     ReportingProducer.reporting[R](internal.produceAsync)(record, attributes)
 
   override def attributes: Map[String, String] = internal.attributes ++ extraAttributes
 
-  override def partitionsFor(topic: String)(implicit trace: Trace): RIO[R with GreyhoundMetrics, Seq[PartitionInfo]] =
+  override def partitionsFor(topic: String): RIO[R with Blocking with Clock with GreyhoundMetrics, Seq[PartitionInfo]] =
     internal
       .partitionsFor(topic)
       .reporting(ProducerGotPartitionsInfo(topic, attributes, _))
 }
 
 object ReportingProducer {
-  type Dependencies = ULayer[GreyhoundMetrics]
+  type Dependencies = ULayer[GreyhoundMetrics with zio.ZEnv]
 
   def apply[R](internal: ProducerR[R], attributes: (String, String)*): ReportingProducer[R] =
     new ReportingProducer(internal, attributes.toMap)
 
   def reporting[R](
     produceAsync: ProducerRecord[Chunk[Byte], Chunk[Byte]] => ZIO[
-      R,
+      Blocking with Clock with R,
       ProducerError,
       IO[ProducerError, RecordMetadata]
     ]
   )(
     record: ProducerRecord[Chunk[Byte], Chunk[Byte]],
     attributes: Map[String, String] = Map.empty
-  ) (implicit trace: Trace): ZIO[GreyhoundMetrics with R, ProducerError, IO[ProducerError, RecordMetadata]] = {
+  ): ZIO[Blocking with Clock with GreyhoundMetrics with R, ProducerError, IO[ProducerError, RecordMetadata]] = {
     for {
       started   <- currentTime(TimeUnit.MILLISECONDS)
-      env       <- ZIO.environment[GreyhoundMetrics]
+      env       <- ZIO.environment[Clock with GreyhoundMetrics with Blocking]
       _         <- GreyhoundMetrics.report(ProducingRecord(record, attributes))
-      onError   <- ZIO.memoize((error: ProducerError) => GreyhoundMetrics.report(ProduceFailed(error, record.topic, attributes)).provideEnvironment(env))
+      onError   <- ZIO.memoize((error: ProducerError) => GreyhoundMetrics.report(ProduceFailed(error, record.topic, attributes)).provide(env))
       onSuccess <-
         ZIO.memoize((metadata: RecordMetadata) =>
           currentTime(TimeUnit.MILLISECONDS)
             .flatMap(ended =>
               GreyhoundMetrics.report(RecordProduced(record, metadata, attributes, FiniteDuration(ended - started, MILLISECONDS)))
             )
-            .provideEnvironment(env)
+            .provide(env)
         )
       promise   <- produceAsync(record).map(_.tapBoth(onError, onSuccess))
     } yield promise
