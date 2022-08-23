2bc33d5f32b3b6ef38669a51ee67761fd3371f38
Noam Berman <noambe@wix.com>
2022-08-23 16:07:56 +0300

    zio 2 (#28812)
    * merge master #draft #draftpr #skipreview #pr
    
    * .
    
    * Update and rename zio2-6_2nd_party_resolved_dependencies.bzl to zio2-7_2nd_party_resolved_dependencies.bzl
    
    * .
    
    * .
    
    * .
    
    * add dynamo streams health test
    
    * .
    
    * fix ht loom
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    GitOrigin-RevId: 9e08ec6f46b94eeb5016fd9bdc58fa101be385df

--- core/src/main/scala/com/wixpress/dst/greyhound/core/producer/Encryptor.scala
+++ core/src/main/scala/com/wixpress/dst/greyhound/core/producer/Encryptor.scala
@@ -1,11 +1,11 @@
 package com.wixpress.dst.greyhound.core.producer
 
-import zio.{Chunk, Task, UIO}
+import zio.{Chunk, Task, Trace, ZIO}
 
 trait Encryptor {
-  def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]): Task[ProducerRecord[K, Chunk[Byte]]]
+  def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]) (implicit trace: Trace): Task[ProducerRecord[K, Chunk[Byte]]]
 }
 
 case object NoOpEncryptor extends Encryptor {
-  override def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]): Task[ProducerRecord[K, Chunk[Byte]]] = UIO(record)
+  override def encrypt[K](record: ProducerRecord[K, Chunk[Byte]]) (implicit trace: Trace): Task[ProducerRecord[K, Chunk[Byte]]] = ZIO.succeed(record)
 }
