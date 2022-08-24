b937867ebb72c8e1dedffe93698bfa50015e85d8
Noam Berman <noambe@wix.com>
2022-08-24 17:43:44 +0300

    ZIO 2 (#28921)
    * Revert "Revert "ZIO 2" (#28897)"
    
    This reverts commit e0a85f8cf80a3e5fb96b858497fe3797222046be.
    
    * remove overrides
    
    * .
    
    * .
    
    * .
    
    GitOrigin-RevId: 0dd63754cc90da9bebf78e70d8d1d1c967d40757

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/TestCtx.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/TestCtx.scala
@@ -2,7 +2,7 @@ package com.wixpress.dst.greyhound.core.testkit
 
 import org.specs2.execute.AsResult
 import zio.test.TestResult
-import zio.{NeedsEnv, RIO, ZIO}
+import zio.{RIO, Trace, ZIO}
 
 object TestCtx {
   def apply[R, CTX](make: => RIO[R, CTX]) = new Wrapper[R, CTX](make)
@@ -14,8 +14,9 @@ object TestCtx {
   }
 
   object Wrapper {
+    implicit private val trace = Trace.empty
     implicit class HasEnvOps[R, CTX](w: Wrapper[R, CTX]) {
-      def apply[R1, E](f: CTX => ZIO[R with R1, E, TestResult])(implicit ev: NeedsEnv[R]) = w.make.flatMap(f)
+      def apply[R1, E](f: CTX => ZIO[R with R1, E, TestResult])/*(implicit ev: NeedsEnv[R])*/ = w.make.flatMap(f)
     }
     implicit class NoEnvOps[CTX](w: Wrapper[Any, CTX])   {
       def apply[R1, E](f: CTX => ZIO[R1, E, TestResult]) = w.make.flatMap(f)
@@ -23,6 +24,7 @@ object TestCtx {
   }
 
   class SpecsWrapper[R, CTX](make: => RIO[R, CTX]) {
+    implicit private val trace = Trace.empty
     def apply[R1, E, A: AsResult](f: CTX => ZIO[R with R1, E, A]) = make.flatMap(f)
   }
 }
