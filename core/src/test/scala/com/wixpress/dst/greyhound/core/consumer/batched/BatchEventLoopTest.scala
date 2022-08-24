783a4524e69d4d06052a45424a595a18651c14f8
Noam Berman <noambe@wix.com>
2022-08-24 17:43:44 +0300

    ZIO 2 (#28921)
    * Revert "Revert "ZIO 2" (#28897)"
    
    This reverts commit e0a85f8cf80a3e5fb96b858497fe3797222046be.
    
    * remove overrides
    
    * .
    
    * .
    
    * .
    
    GitOrigin-RevId: 0dd63754cc90da9bebf78e70d8d1d1c967d40757

--- core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/batched/BatchEventLoopTest.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/batched/BatchEventLoopTest.scala
@@ -10,24 +10,24 @@ import com.wixpress.dst.greyhound.core.consumer.{Consumer, DelayedRebalanceEffec
 import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.core.testkit.TestMetrics
 import com.wixpress.dst.greyhound.core.testkit.{AwaitableRef, TestCtx}
-import zio.blocking.Blocking
-import zio.duration._
+
 import zio.test.Assertion._
 import zio.test._
-import zio.test.environment.Live.live
-import zio.test.environment.TestClock
 import zio.test.junit.JUnitRunnableSpec
-import zio.{Cause, Chunk, IO, Queue, RIO, Ref, Task, UIO, ZIO}
+import zio.{Cause, Chunk, IO, Queue, RIO, Ref, Task, ZIO}
+import zio._
 
 class BatchEventLoopTest extends JUnitRunnableSpec {
+  private implicit val trace = zio.Trace.empty
+
   def spec = suite("BatchEventLoopTest")(
-    testM("successfully consume a batch of messages and commit offsets") {
+    test("successfully consume a batch of messages and commit offsets") {
       inCtx { c =>
         import c._
         val consumerRecords = records()
         val topics          = consumerRecords.map(_.topic).distinct
         val topicPartitions = consumerRecords.map(_.topicPartition).distinct
-        BatchEventLoop.make(group, ConsumerSubscription.topics(topics: _*), consumer, handler, clientId).use { _ =>
+        ZIO.scoped(BatchEventLoop.make(group, ConsumerSubscription.topics(topics: _*), consumer, handler, clientId).flatMap { _ =>
           for {
             _              <- givenRecords(consumerRecords)
             handledRecords <- handled.await(_.size >= topicPartitions.size)
@@ -39,73 +39,81 @@ class BatchEventLoopTest extends JUnitRunnableSpec {
             assert(metrics)(exists(isRecordsHandledMetric(topics, group, clientId))) &&
             assert(metrics)(exists(isBatchHandledMetric(group, clientId)))
           }
-        }
+        })
       }
     },
-    testM("drop failed topic-partition records, if no retry") {
-      checkAllM(noRetry) {
+    test("drop failed topic-partition records, if no retry") {
+      checkAll(noRetry) {
         case (retry, cause) =>
           inCtx { c =>
             import c._
             val consumerRecords = records(topicCount = 1, partitions = 2)
             val topics          = consumerRecords.map(_.topic).distinct
-            BatchEventLoop.make(group, ConsumerSubscription.topics(topics: _*), consumer, handler, clientId, retry).use { loop =>
-              for {
-                _              <- UIO(println(s"Should not retry for retry: $retry, cause: $cause"))
-                _              <- givenHandleError(failOnPartition(0, cause))
-                _              <- givenRecords(consumerRecords)
-                handledRecords <- handled.await(_.nonEmpty)
-                offsets        <- committedOffsetsRef.await(_.nonEmpty)
-                _              <- live(zio.clock.sleep(500.millis))
-                state          <- loop.state
-              } yield {
-                assert(handledRecords)(equalTo(Vector(consumerRecords.filterNot(_.partition == 0)))) &&
-                assert(offsets)(equalTo(consumerRecords.groupBy(_.topicPartition).mapValues(_.map(_.offset + 1).max))) &&
-                assert(state.pendingRecords.values.sum)(equalTo(0))
-              }
-            }
+            ZIO.scoped(BatchEventLoop.make(group, ConsumerSubscription.topics(topics: _*), consumer, handler, clientId, retry).flatMap {
+              loop =>
+                for {
+                  _              <- ZIO.succeed(println(s"Should not retry for retry: $retry, cause: $cause"))
+                  _              <- givenHandleError(failOnPartition(0, cause))
+                  _              <- givenRecords(consumerRecords)
+                  handledRecords <- handled.await(_.nonEmpty)
+                  offsets        <- committedOffsetsRef.await(_.nonEmpty)
+                  _              <- TestClock.adjust(500.millis)
+                  state          <- loop.state
+                } yield {
+                  assert(handledRecords)(equalTo(Vector(consumerRecords.filterNot(_.partition == 0)))) &&
+                  assert(offsets)(equalTo(consumerRecords.groupBy(_.topicPartition).mapValues(_.map(_.offset + 1).max))) &&
+                  assert(state.pendingRecords.values.sum)(equalTo(0))
+                }
+            })
           }
       }
     },
-    testM("retry on failed topic-partition records, if retry configured") {
-      checkAllM(retryCauses) { cause =>
+    test("retry on failed topic-partition records, if retry configured") {
+      checkAll(retryCauses) { cause =>
         inCtx { c =>
           import c._
           val consumerRecords = records(topicCount = 1, partitions = 2)
           val topics          = consumerRecords.map(_.topic).distinct
           val retry           = BatchRetryConfig(backoff = 1.second)
 
-          BatchEventLoop.make(group, ConsumerSubscription.topics(topics: _*), consumer, handler, clientId, Some(retry)).use { loop =>
-            for {
-              _        <- UIO(println(s"Should retry for cause: $cause"))
-              _        <- givenHandleError(failOnPartition(0, cause))
-              _        <- givenRecords(consumerRecords)
-              handled1 <- handled.await(_.nonEmpty)
-              offsets1 <- committedOffsetsRef.await(_.nonEmpty)
-              _        <- live(zio.clock.sleep(500.millis))
-              state1   <- loop.state
-              _        <- givenHandleError(_ => None)
+          ZIO.scoped(BatchEventLoop.make(group, ConsumerSubscription.topics(topics: _*), consumer, handler, clientId, Some(retry)).flatMap {
+            loop =>
+              for {
+                _        <- ZIO.succeed(println(s"Should retry for cause: $cause"))
+                _        <- givenHandleError(failOnPartition(0, cause))
+                _        <- givenRecords(consumerRecords)
+                handled1 <- handled.await(_.nonEmpty)
+                offsets1 <- committedOffsetsRef.await(_.nonEmpty)
+                _        <- TestClock.adjust(500.millis)
+                state1   <- loop.state
+                _        <- givenHandleError(_ => None)
 
-              _ <- TestClock.adjust(retry.backoff + 1.milli)
+                _ <- TestClock.adjust(retry.backoff + 1.milli)
 
-              handled2 <- handled.await(_.size > 1)
-              offsets2 <- committedOffsetsRef.await(_.size > 1)
-              state2   <- loop.state
-            } yield {
-              val goodRecords = consumerRecords.filterNot(_.partition == 0)
-              assert(handled1)(equalTo(Vector(goodRecords))) &&
-              assert(offsets1)(equalTo(goodRecords.groupBy(_.topicPartition).mapValues(_.map(_.offset + 1).max))) &&
-              assert(state1.pendingRecords.values.sum)(equalTo(goodRecords.size)) &&
-              // -- after partition 0 handling succeeds
-              assert(handled2)(containsRecordsByPartition(consumerRecords)) &&
-              assert(offsets2)(equalTo(consumerRecords.groupBy(_.topicPartition).mapValues(_.map(_.offset + 1).max))) &&
-              assert(state2.pendingRecords.values.sum)(equalTo(0))
-            }
-          }
+                handled2 <- handled.await(_.size > 1)
+                offsets2 <- committedOffsetsRef.await(_.size > 1)
+                state2   <- loop.state
+              } yield {
+                val goodRecords = consumerRecords.filterNot(_.partition == 0)
+                assert(handled1)(equalTo(Vector(goodRecords))) &&
+                assert(offsets1)(equalTo(goodRecords.groupBy(_.topicPartition).mapValues(_.map(_.offset + 1).max))) &&
+                assert(state1.pendingRecords.values.sum)(equalTo(goodRecords.size)) &&
+                // -- after partition 0 handling succeeds
+                assert(handled2)(containsRecordsByPartition(consumerRecords)) &&
+                assert(offsets2)(equalTo(consumerRecords.groupBy(_.topicPartition).mapValues(_.map(_.offset + 1).max))) &&
+                assert(state2.pendingRecords.values.sum)(equalTo(0))
+              }
+          })
         }
       }
     }
-  ).provideCustomLayer(TestMetrics.makeLayer)
+  ).provideEnvironment(environment)
+
+  private def environment: ZEnvironment[TestMetrics with Scope with TestEnvironment] =
+    TestMetrics.buildEnv ++
+      zio.Unsafe.unsafe { implicit s => runtime.unsafe.run(
+        ZIO.scoped((zio.test.testEnvironment.>+>(TestClock.default) ++ ZLayer.succeed(Scope.global : Scope)).build)
+      ).getOrThrowFiberFailure() }
 
   private def isRecordsHandledMetric(topics: Seq[Topic], group: String, clientId: String) =
     isSubtype[RecordsHandled[_, _]](
@@ -140,28 +148,35 @@ class BatchEventLoopTest extends JUnitRunnableSpec {
     val group, clientId = randomStr
 
     val consumer = new EmptyConsumer {
-      override def poll(timeout: Duration): Task[Records] =
+      override def poll(timeout: Duration)(implicit trace: Trace): Task[Records] =
         queue.take
           .timeout(timeout)
-          .provideLayer(zio.clock.Clock.live)
           .map(_.getOrElse(Iterable.empty))
-          .tap(r => UIO(println(s"poll($timeout): $r")))
+          .tap(r => ZIO.succeed(println(s"poll($timeout): $r")))
 
-      override def commit(offsets: Map[TopicPartition, Offset]): Task[Unit] = {
-        UIO(println(s"commit($offsets)")) *> committedOffsetsRef.update(_ ++ offsets)
+      override def commit(offsets: Map[TopicPartition, Offset])(implicit trace: Trace): Task[Unit] = {
+        ZIO.succeed(println(s"commit($offsets)")) *> committedOffsetsRef.update(_ ++ offsets)
       }
 
-      override def commitOnRebalance(offsets: Map[TopicPartition, Offset]): RIO[Blocking with GreyhoundMetrics, DelayedRebalanceEffect] = {
-        ZIO.runtime[Any].flatMap { rt => UIO(DelayedRebalanceEffect(rt.unsafeRunTask(committedOffsetsRef.update(_ ++ offsets)))) }
+      override def commitOnRebalance(
+        offsets: Map[TopicPartition, Offset]
+      )(implicit trace: Trace): RIO[GreyhoundMetrics, DelayedRebalanceEffect] = {
+        ZIO.runtime[Any].flatMap { rt =>
+          ZIO.succeed(
+            DelayedRebalanceEffect(
+              zio.Unsafe.unsafe { implicit s => rt.unsafe.run(committedOffsetsRef.update(_ ++ offsets)).getOrThrowFiberFailure() }
+            )
+          )
+        }
       }
     }
     val handler  = new BatchRecordHandler[Any, Throwable, Chunk[Byte], Chunk[Byte]] {
       override def handle(records: RecordBatch): ZIO[Any, HandleError[Throwable], Any] = {
-        UIO(println(s"handle($records)")) *>
-          (handlerErrorsRef.get.flatMap(he => he(records.records).fold(ZIO.unit: IO[HandleError[Throwable], Unit])(ZIO.halt(_))) *>
+        ZIO.succeed(println(s"handle($records)")) *>
+          (handlerErrorsRef.get.flatMap(he => he(records.records).fold(ZIO.unit: IO[HandleError[Throwable], Unit])(ZIO.failCause(_))) *>
             handled.update(_ :+ records.records))
-            .tapCause(e => UIO(println(s"handle failed with $e, records: $records")))
-            .tap(_ => UIO(println(s"handled $records")))
+            .tapErrorCause(e => ZIO.succeed(println(s"handle failed with $e, records: $records")))
+            .tap(_ => ZIO.succeed(println(s"handled $records")))
       }
     }
 
