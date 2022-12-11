6b35942105f55e8818223c8cbd4cae3429e06f9c
amirh <amirh@wix.com>
2022-12-11 18:04:36 +0200

    compiles

--- greyhoundsidecar/src/main/scala/greyhound/DebugMetrics.scala
+++ greyhoundsidecar/src/main/scala/greyhound/DebugMetrics.scala
@@ -1,8 +1,7 @@
 package greyhound
 
-import com.wixpress.dst.greyhound.core.consumer.ConsumerMetric.PolledRecords
 import com.wixpress.dst.greyhound.core.metrics.{GreyhoundMetric, GreyhoundMetrics}
-import zio.{URIO, ZIO, ZLayer}
+import zio.{Trace, UIO, ZIO, ZLayer}
 
 
 object DebugMetrics {
@@ -10,9 +9,10 @@ object DebugMetrics {
 
   val layer = ZLayer.succeed(report(metric => println(s"-Metrics- $metric")))
 
-  private def report(report: GreyhoundMetric => Unit): GreyhoundMetrics.Service  = {
-    case _: PolledRecords => ZIO.unit
-    case metric: GreyhoundMetric => ZIO.succeed(report(metric))
+  private def report(reportMetric: GreyhoundMetric => Unit): GreyhoundMetrics.Service  = {
+    new GreyhoundMetrics.Service {
+      override def report(metric: GreyhoundMetric)(implicit trace: Trace): UIO[Unit] = ZIO.succeed(reportMetric(metric))
+    }
   }
 
 }
