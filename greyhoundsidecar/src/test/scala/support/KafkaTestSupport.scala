7c07f3a1377b29602316127e44dc3616c1a0df78
dmitrychait <dmitryc@wix.com>
2023-01-22 10:50:04 +0200

    greyhound-sidecar-remove-closedkafka

--- greyhoundsidecar/src/test/scala/support/KafkaTestSupport.scala
+++ greyhoundsidecar/src/test/scala/support/KafkaTestSupport.scala
@@ -2,7 +2,7 @@ package support
 
 import com.wixpress.dst.greyhound.testkit.{ManagedKafka, ManagedKafkaConfig}
 import greyhound.DebugMetrics
-import zio.ZLayer
+import zio.{Fiber, ZIO, ZLayer}
 import zio.test.TestAspect
 import zio.test.TestAspect.{afterAll, beforeAll}
 
@@ -12,15 +12,11 @@ trait KafkaTestSupport {
   val zooKeeperPort = 2181
 
   val kafkaConfig: ManagedKafkaConfig = ManagedKafkaConfig(kafkaPort, zooKeeperPort, Map.empty)
-  val managedKafka = ManagedKafka.make(kafkaConfig).forever.forkDaemon
+  val managedKafka = ManagedKafka.make(kafkaConfig).interruptible.forkScoped
     .provideSomeLayer(DebugMetrics.layer ++ ZLayer.succeed(zio.Scope.global))
 
 
   def runKafka: TestAspect[Nothing, Any, Throwable, Any] = {
     beforeAll(zio.Console.printLine("staring kafka!!!").ignore *> managedKafka)
-  }
-
-  def closeKafka: TestAspect[Nothing, Any, Nothing, Any] = {
-    afterAll(zio.Console.printLine("closing kafka!!!").ignore *> managedKafka.map(_.interrupt))
   }
 }
