3fd6ce5cd5e1e897ed3b790553ec314b41dae940
dmitrychait <dmitryc@wix.com>
2023-01-23 16:12:24 +0200

    sidecar test refactor

--- greyhoundsidecar/src/test/scala/sidecaruser/FailOnceTestSidecarUser.scala
+++ greyhoundsidecar/src/test/scala/sidecaruser/FailOnceTestSidecarUser.scala
@@ -5,15 +5,19 @@ import com.wixpress.dst.greyhound.sidecar.api.v1.greyhoundsidecaruser.{HandleMes
 import io.grpc.Status
 import zio.{Ref, ZIO}
 
-class FailOnceSidecarUserService(consumedTopics: Ref[Seq[HandleMessagesRequest]], shouldFailRef: Ref[Boolean]) extends RGreyhoundSidecarUser[Any] {
+class FailOnceTestSidecarUser(consumedTopics: Ref[Seq[HandleMessagesRequest]],
+                              shouldFailRef: Ref[Boolean]) extends RGreyhoundSidecarUser[Any] {
 
-  def collectedRecords: Ref[Seq[HandleMessagesRequest]] = consumedTopics
+  def collectedRecords = consumedTopics.get
 
   override def handleMessages(request: HandleMessagesRequest): ZIO[Any, Status, HandleMessagesResponse] = {
     for {
       shouldFail <- shouldFailRef.get
-      _ <- if (shouldFail) shouldFailRef.update(_ => false) *> ZIO.fail(Status.DATA_LOSS)
-      else consumedTopics.update(_ :+ request)
+      _ <- if (shouldFail)  {
+        shouldFailRef.update(_ => false) *> ZIO.fail(Status.DATA_LOSS)
+      } else  {
+        consumedTopics.update(_ :+ request)
+      }
     } yield HandleMessagesResponse()
   }
 }
