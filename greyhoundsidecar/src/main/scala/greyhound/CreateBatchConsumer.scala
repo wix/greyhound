78c60403c4f5ca90cf97a86a9abae433dcf3e74e
dmitrychait <dmitryc@wix.com>
2023-01-24 09:45:46 +0200

    batch-consumer-fix-and-tests

--- greyhoundsidecar/src/main/scala/greyhound/CreateBatchConsumer.scala
+++ greyhoundsidecar/src/main/scala/greyhound/CreateBatchConsumer.scala
@@ -4,10 +4,12 @@ import com.wixpress.dst.greyhound.core.Serdes
 import com.wixpress.dst.greyhound.core.consumer.OffsetReset
 import com.wixpress.dst.greyhound.core.consumer.batched._
 import com.wixpress.dst.greyhound.core.consumer.domain._
+import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetrics
 import com.wixpress.dst.greyhound.sidecar.api.v1.greyhoundsidecar.BatchConsumer.RetryStrategy
 import com.wixpress.dst.greyhound.sidecar.api.v1.greyhoundsidecar.BatchConsumer.RetryStrategy.Blocking
 import greyhound.BatchRetryStrategyMapper.asRetryConfig
-import zio.ZIO
+import greyhound.Register.Register
+import zio.{Scope, ZLayer}
 
 import scala.concurrent.duration.DurationInt
 
@@ -16,23 +18,19 @@ object CreateBatchConsumer {
   def apply(topic: String, group: String, retryStrategy: RetryStrategy, kafkaAddress: String) =
     for {
       managedClient <- SidecarUserClient.managed
-      _             <- ZIO.scoped { // TODO: Fix scoped to be like CreateConsumer + add appropriate test
-        managedClient
-          .flatMap(client =>
-            BatchConsumer.make(
-              config = BatchConsumerConfig(
-                bootstrapServers = kafkaAddress,
-                groupId = group,
-                offsetReset = OffsetReset.Earliest,
-                initialSubscription = ConsumerSubscription.Topics(Set(topic)),
-                retryConfig = asRetryConfig(retryStrategy)
-              ),
-              handler = BatchConsumerHandler(topic, group, client)
-                .withDeserializers(Serdes.StringSerde, Serdes.StringSerde)
-            )
-          )
-          .forever
-      }
+      _ <- managedClient.flatMap(client =>
+        BatchConsumer.make(
+          config = BatchConsumerConfig(
+            bootstrapServers = kafkaAddress,
+            groupId = group,
+            offsetReset = OffsetReset.Earliest,
+            initialSubscription = ConsumerSubscription.Topics(Set(topic)),
+            retryConfig = asRetryConfig(retryStrategy)
+          ),
+          handler = BatchConsumerHandler(topic, group, client)
+            .withDeserializers(Serdes.StringSerde, Serdes.StringSerde)
+        )
+      ).provideSomeLayer[GreyhoundMetrics with Register](ZLayer.succeed(Scope.global))
     } yield ()
 
 }
@@ -41,6 +39,6 @@ object BatchRetryStrategyMapper {
   def asRetryConfig(retryStrategy: RetryStrategy): Option[BatchRetryConfig] =
     retryStrategy match {
       case Blocking(value) => Some(BatchRetryConfig.infiniteBlockingRetry(value.interval.millis))
-      case _               => None
+      case _ => None
     }
 }
