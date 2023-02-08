62b7cf3600ce3782462b681a7049381544612389
Avigdor Mansbach <avigdorm@wix.com>
2023-02-08 11:45:30 +0200

    test + start impl

--- greyhoundsidecar/src/main/scala/greyhound/Registry.scala
+++ greyhoundsidecar/src/main/scala/greyhound/Registry.scala
@@ -13,6 +13,8 @@ trait Registry {
 
   def tenantExists(tenantId: String): Task[Boolean]
 
+  def markTenantAsDead(tenantId: String): Task[Unit]
+
   def addConsumer(tenantId: String, topic: String, consumerGroup: String, shutdown: RIO[Env, Unit]): Task[Unit]
 
   def removeConsumer(tenantId: String, topic: String, consumerGroup: String): Task[Unit]
@@ -21,15 +23,26 @@ trait Registry {
 
   def isUniqueConsumer(topic: String, consumerGroup: String, tenantId: String): UIO[Boolean]
 }
+
 case class TenantRegistry(ref: Ref[Map[String, TenantInfo]]) extends Registry {
   override def addTenant(tenantId: String, host: String, port: Int): Task[Unit] =
-    ref.update(_.updated(tenantId, TenantInfo(hostDetails = TenantHostDetails(host, port), consumers = Map.empty)))
+    ref.update(_.updated(tenantId, TenantInfo(hostDetails = TenantHostDetails(host, port, alive = true), consumers = Map.empty)))
 
   override def removeTenant(tenantId: String): Task[Unit] =
     ref.update(_ - tenantId)
 
   override def getTenant(tenantId: String): UIO[Option[TenantInfo]] =
     ref.get.map(_.get(tenantId))
+
+  override def markTenantAsDead(tenantId: String): Task[Unit] =
+    ref.update { tenants =>
+      tenants.get(tenantId) match {
+        case Some(tenant) =>
+          tenants.updated(tenantId, tenant.copy(hostDetails = tenant.hostDetails.copy(alive = false)))
+        case None =>
+          tenants
+      }
+    }
 
   override def addConsumer(tenantId: String, topic: String, consumerGroup: String, shutdown: RIO[Env, Unit]): Task[Unit] =
     ref.update { tenants =>
@@ -70,7 +83,7 @@ object TenantRegistry {
 
 case class TenantInfo(hostDetails: TenantHostDetails, consumers: Map[(String, String), TenantConsumerInfo])
 
-case class TenantHostDetails(host: String, port: Int)
+case class TenantHostDetails(host: String, port: Int, alive: Boolean)
 
 case class TenantConsumerInfo(topic: String, consumerGroup: String, shutdown: RIO[Env, Unit])
 
