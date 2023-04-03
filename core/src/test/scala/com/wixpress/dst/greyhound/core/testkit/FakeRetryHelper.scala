2a49a320443156dc527450475fb952a36ce69893
Natan Silnitsky <natans@wix.com>
2023-04-03 15:22:47 +0300

    Revert "Blocking retries attempts tracking fix" (#33818)
    Revert "Blocking retries attempts tracking fix (#32757)"
    
    This reverts commit 7e0e8cbccdd4c5fe6446775262b6695697785b2b.
    
    GitOrigin-RevId: eb859fcf70860c06a0bd0492b746ed3ff00bc8ac

--- core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/FakeRetryHelper.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/testkit/FakeRetryHelper.scala
@@ -1,13 +1,18 @@
 package com.wixpress.dst.greyhound.core.testkit
 
 import java.time.Instant
+import java.util.concurrent.TimeUnit.MILLISECONDS
+
+import com.wixpress.dst.greyhound.core.Serdes._
 import com.wixpress.dst.greyhound.core._
 import com.wixpress.dst.greyhound.core.consumer.domain.{ConsumerRecord, ConsumerSubscription}
 import com.wixpress.dst.greyhound.core.consumer.retry.RetryDecision.{NoMoreRetries, RetryWith}
-import com.wixpress.dst.greyhound.core.consumer.retry.{BlockingHandlerFailed, NonBlockingRetryHelper, RetryAttempt, RetryDecision, RetryHeader}
+import com.wixpress.dst.greyhound.core.consumer.retry.{BlockingHandlerFailed, NonBlockingRetryHelper, RetryAttempt, RetryDecision}
 import com.wixpress.dst.greyhound.core.producer.ProducerRecord
 import com.wixpress.dst.greyhound.core.testkit.FakeRetryHelper._
 import zio._
+import zio.Clock
+
 import zio.Clock
 
 trait FakeNonBlockingRetryHelper extends NonBlockingRetryHelper {
@@ -15,6 +20,15 @@ trait FakeNonBlockingRetryHelper extends NonBlockingRetryHelper {
 
   override def retryTopicsFor(originalTopic: Topic): Set[Topic] =
     Set(s"$originalTopic-retry")
+
+  override def retryAttempt(topic: Topic, headers: Headers, subscription: ConsumerSubscription)(
+    implicit trace: Trace
+  ): UIO[Option[RetryAttempt]] =
+    (for {
+      attempt     <- headers.get(Header.Attempt, IntSerde)
+      submittedAt <- headers.get(Header.SubmittedAt, InstantSerde)
+      backoff     <- headers.get(Header.Backoff, DurationSerde)
+    } yield retryAttemptInternal(topic, attempt, submittedAt, backoff)).orElse(ZIO.none)
 
   override def retryDecision[E](
     retryAttempt: Option[RetryAttempt],
@@ -24,23 +38,35 @@ trait FakeNonBlockingRetryHelper extends NonBlockingRetryHelper {
   )(implicit trace: Trace): URIO[Any, RetryDecision] =
     error match {
       case RetriableError | BlockingHandlerFailed =>
-        currentTime.map(now =>
-          RetryWith(recordFrom(now, retryAttempt, record))
+        currentTime.flatMap(now =>
+          recordFrom(now, retryAttempt, record)
+            .fold(_ => NoMoreRetries, RetryWith)
         )
       case NonRetriableError                      =>
         ZIO.succeed(NoMoreRetries)
     }
 
+  private def retryAttemptInternal(topic: Topic, attempt: Option[Int], submittedAt: Option[Instant], backoff: Option[Duration]) =
+    for {
+      a <- attempt
+      s <- submittedAt
+      b <- backoff
+    } yield RetryAttempt(topic, a, s, b)
+
   private def recordFrom(now: Instant, retryAttempt: Option[RetryAttempt], record: ConsumerRecord[Chunk[Byte], Chunk[Byte]])(
     implicit trace: Trace
   ) = {
     val nextRetryAttempt = retryAttempt.fold(0)(_.attempt + 1)
-    ProducerRecord(
+    for {
+      retryAttempt <- IntSerde.serialize(topic, nextRetryAttempt)
+      submittedAt  <- InstantSerde.serialize(topic, now)
+      backoff      <- DurationSerde.serialize(topic, 1.second)
+    } yield ProducerRecord(
       topic = s"$topic-retry",
       value = record.value,
       key = record.key,
       partition = None,
-      headers = RetryAttempt.toHeaders(RetryAttempt(topic, nextRetryAttempt, now, 1.second))
+      headers = Headers(Header.Attempt -> retryAttempt, Header.SubmittedAt -> submittedAt, Header.Backoff -> backoff)
     )
   }
 }
@@ -49,8 +75,13 @@ case class FakeRetryHelper(topic: Topic) extends FakeNonBlockingRetryHelper
 
 object FakeRetryHelper {
   implicit private val trace = Trace.empty
+  object Header {
+    val Attempt     = "retry-attempt"
+    val SubmittedAt = "retry-submitted-at"
+    val Backoff     = "retry-backoff"
+  }
 
-  val currentTime: UIO[Instant] = Clock.instant
+  val currentTime = Clock.currentTime(MILLISECONDS).map(Instant.ofEpochMilli)
 }
 
 sealed trait HandlerError
