648bf56a5875fccdb1673c027ba90b9fe10b3768
Ben Wattelman <82799628+ben-wattelman@users.noreply.github.com>
2023-04-04 12:03:59 +0300

    [greyhound] parallel consumer OffsetsAndGaps (#33605)
    GitOrigin-RevId: f178c94663c7cbcb22bd7266c3e15919d8997d8c

---
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/OffsetsAndGapsTest.scala
@@ -0,0 +1,62 @@
+package com.wixpress.dst.greyhound.core.consumer
+
+import com.wixpress.dst.greyhound.core.TopicPartition
+import com.wixpress.dst.greyhound.core.consumer.OffsetGapsTest._
+import com.wixpress.dst.greyhound.core.testkit.BaseTestNoEnv
+
+class OffsetsAndGapsTestGapsTest extends BaseTestNoEnv {
+
+  "calculate gaps created by handled batch" in {
+    for {
+      offsetGaps  <- OffsetsAndGaps.make
+      _           <- offsetGaps.update(topicPartition, Seq(1L, 3L, 7L))
+      currentGaps <- offsetGaps.gapsForPartition(topicPartition)
+    } yield currentGaps must beEqualTo(Seq(Gap(0L, 0L), Gap(2L, 2L), Gap(4L, 6L)))
+  }
+
+  "update offset and gaps according to handled batch" in {
+    for {
+      offsetGaps             <- OffsetsAndGaps.make
+      _                      <- offsetGaps.update(topicPartition, Seq(1L, 3L, 7L))
+      _                      <- offsetGaps.update(topicPartition, Seq(2L, 5L))
+      getCommittableAndClear <- offsetGaps.getCommittableAndClear
+    } yield getCommittableAndClear must havePair(topicPartition -> OffsetAndGaps(7L, Seq(Gap(0L, 0L), Gap(4L, 4L), Gap(6L, 6L))))
+  }
+
+  "clear committable offsets" in {
+    for {
+      offsetGaps             <- OffsetsAndGaps.make
+      _                      <- offsetGaps.update(topicPartition, Seq(1L, 3L, 7L))
+      _                      <- offsetGaps.getCommittableAndClear
+      getCommittableAndClear <- offsetGaps.getCommittableAndClear
+    } yield getCommittableAndClear must beEmpty
+  }
+
+  "do not clear gaps on retrieving current" in {
+    for {
+      offsetGaps  <- OffsetsAndGaps.make
+      _           <- offsetGaps.update(topicPartition, Seq(1L, 3L, 7L))
+      _           <- offsetGaps.gapsForPartition(topicPartition)
+      currentGaps <- offsetGaps.gapsForPartition(topicPartition)
+    } yield currentGaps must beEqualTo(Seq(Gap(0L, 0L), Gap(2L, 2L), Gap(4L, 6L)))
+  }
+
+  "update with larger offset" in {
+    val partition0 = TopicPartition(topic, 0)
+    val partition1 = TopicPartition(topic, 1)
+
+    for {
+      offsetGaps <- OffsetsAndGaps.make
+      _          <- offsetGaps.update(partition0, Seq(1L))
+      _          <- offsetGaps.update(partition0, Seq(0L))
+      _          <- offsetGaps.update(partition1, Seq(0L))
+      current    <- offsetGaps.getCommittableAndClear
+    } yield current must havePairs(partition0 -> OffsetAndGaps(1L, Seq()), partition1 -> OffsetAndGaps(0L, Seq()))
+  }
+
+}
+
+object OffsetGapsTest {
+  val topic          = "some-topic"
+  val topicPartition = TopicPartition(topic, 0)
+}
