fbd35427a598c9f99e57cb3e0cf5a6dc02a1bde9
Ben Wattelman <82799628+ben-wattelman@users.noreply.github.com>
2023-06-06 16:06:05 +0300

    [greyhound] parallel consumer - add offsets and gaps init (#35027)
    GitOrigin-RevId: fe53459fd02e8e627356cbe68d71e4d4bc6b3d24

--- core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/OffsetsAndGapsTest.scala
+++ core/src/test/scala/com/wixpress/dst/greyhound/core/consumer/OffsetsAndGapsTest.scala
@@ -1,6 +1,8 @@
 package com.wixpress.dst.greyhound.core.consumer
 
 import com.wixpress.dst.greyhound.core.TopicPartition
+import com.wixpress.dst.greyhound.core.consumer.Gap.GAP_SEPARATOR
+import com.wixpress.dst.greyhound.core.consumer.OffsetAndGaps.LAST_HANDLED_OFFSET_SEPARATOR
 import com.wixpress.dst.greyhound.core.consumer.OffsetGapsTest._
 import com.wixpress.dst.greyhound.core.testkit.BaseTestNoEnv
 
@@ -52,6 +54,26 @@ class OffsetsAndGapsTestGapsTest extends BaseTestNoEnv {
       _          <- offsetGaps.update(partition1, Seq(0L))
       current    <- offsetGaps.getCommittableAndClear
     } yield current must havePairs(partition0 -> OffsetAndGaps(1L, Seq()), partition1 -> OffsetAndGaps(0L, Seq()))
+  }
+
+  "init with given offsets and calculate subsequent gaps accordingly" in {
+    val partition0              = TopicPartition(topic, 0)
+    val partition1              = TopicPartition(topic, 1)
+    val initialCommittedOffsets =
+      Map(partition0 -> OffsetAndGaps(100L, committable = false), partition1 -> OffsetAndGaps(200L, committable = false))
+    for {
+      offsetGaps <- OffsetsAndGaps.make
+      _          <- offsetGaps.init(initialCommittedOffsets)
+      _          <- offsetGaps.update(partition0, Seq(101L, 102L))
+      _          <- offsetGaps.update(partition1, Seq(203L, 204L))
+      current    <- offsetGaps.getCommittableAndClear
+    } yield current must havePairs(partition0 -> OffsetAndGaps(102L, Seq()), partition1 -> OffsetAndGaps(204L, Seq(Gap(201L, 202L))))
+  }
+
+  "parse gaps from string" in {
+    val gaps     = Seq(s"10${LAST_HANDLED_OFFSET_SEPARATOR}0${GAP_SEPARATOR}1", s"10${LAST_HANDLED_OFFSET_SEPARATOR}", "")
+    val expected = Seq(Some(OffsetAndGaps(10, Seq(Gap(0, 1)))), Some(OffsetAndGaps(10, Seq())), None)
+    gaps.map(OffsetsAndGaps.parseGapsString).must(beEqualTo(expected))
   }
 
 }
